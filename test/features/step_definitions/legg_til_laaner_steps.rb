eb20a2194f27557df336af25dd119bbb0380926c
Benjamin Rokseth <bensinober@gmail.com>
2014-08-15 14:54:06 +0200

    Fix step_definition legg_til_laaner

--- test/features/step_definitions/legg_til_laaner_steps.rb
+++ test/features/step_definitions/legg_til_laaner_steps.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 
-Gitt(/^at det finnes en admin\-bruker$/) do
+Gitt(/^at admin\-bruker er logget inn$/) do
   pending # express the regexp above with the code you wish you had
 end
 
