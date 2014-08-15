9478274abc3362e8527a6a3115be96d607305d9a
Asgeir Rekkavik <arekkavik@gmail.com>
2014-08-15 14:26:45 +0200

    Change "bruker" to "låner"

--- test/features/step_definitions/legg_til_laaner_steps.rb
+++ test/features/step_definitions/legg_til_laaner_steps.rb
@@ -4,10 +4,10 @@ Gitt(/^at det finnes en admin\-bruker$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-Når(/^Admin\-bruker legger inn "(.*?)" som ny bruker$/) do |arg1|
+Når(/^Admin\-bruker legger inn "(.*?)" som ny låner$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Så(/^registrerer systemet at "(.*?)" er bruker$/) do |arg1|
+Så(/^registrerer systemet at "(.*?)" er låner$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
