1b6f307176cca280e1b06616dbbc7a669ac5a21e
akafred <github@akafred.com>
2014-08-19 18:17:22 +0200

    Make after scenario hooks more robust.
    - Added comment about ordering of hooks.
    - A separate hook for closing browser.
    - Avoid issue when deleting library if there are more than one defined.

--- test/features/support/hooks.rb
+++ test/features/support/hooks.rb
@@ -21,22 +21,30 @@ def add_screenshot(name)
   embed filename, 'image/png'
 end
 
+# BEFORE HOOKS will run in the same order of which they are registered.
+
 Before do
   @context = {}
 end
 
-After do |scenario|
-  if scenario.failed? && @browser
-    add_screenshot(scenario.title)
-  end
+#  AFTER HOOKS will run in the opposite order of which they are registered.
+
+After do # The final hook
   @browser.close if @browser
 end
 
+After do |scenario|
+  add_screenshot(scenario.title) if scenario.failed? && @browser
+end
+
 After('@libraryCreated') do
-  table = @browser.table(:id, "branchest")
+  @browser.goto intranet(:branches)
+  table = @browser.table(:id => "branchest")
+  table.wait_until_present
   table.rows.each do | row |
     if row.text.include?("#{@context[:branchcode]}")
       row.link(:href => /op=delete/).click
+      break # the click will cause navigation so iterating more might fail
     end
   end
   form = @browser.form(:action => "/cgi-bin/koha/admin/branches.pl")
