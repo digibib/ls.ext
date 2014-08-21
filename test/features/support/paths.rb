c7146bf2ff485a61c8f08f772e1fb323da7834a9
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-08-21 08:09:20 +0200

    Refactor Koha paths support code.
    Makes it possible to access the host adress and port separatly.

--- test/features/support/paths.rb
+++ test/features/support/paths.rb
@@ -1,7 +1,14 @@
 class Paths
 
+  def host
+    "192.168.50.10"
+  end
+
+  def port
+    8081
+  end
+
   def intranet(path=nil)
-    prefix = "http://192.168.50.10:8081"
     paths = {
       :home     => "/",
       :branches => "/cgi-bin/koha/admin/branches.pl",
@@ -9,7 +16,7 @@ class Paths
       :patrons => "/cgi-bin/koha/members/members-home.pl",
       :admin    => "/cgi-bin/koha/admin/admin-home.pl"
       }
-    raise ArgumentError, "Invalid or missing path argument" unless path && paths[path.to_sym] 
-    return prefix + paths[path.to_sym]
+    raise ArgumentError, "Invalid or missing path argument" unless path && paths[path.to_sym]
+    return "http://#{host}:#{port}#{paths[path.to_sym]}"
   end
 end
