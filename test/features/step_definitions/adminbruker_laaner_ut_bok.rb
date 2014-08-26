55d1e38bdcc292d48b71bd9c5f55b67f7f9a5e16
Asgeir Rekkavik <arekkavik@gmail.com>
2014-08-26 11:30:06 +0200

    Add step definitions for feature "adminbruker_laaner_ut_bok"

---
+++ test/features/step_definitions/adminbruker_laaner_ut_bok.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+
+Given(/^at jeg er logget inn som adminbruker/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+Given(/^at "(.*?)" eksisterer som en låner/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+Given(/^at "(.*?)" er ei bok som finnes i biblioteket/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+When(/^jeg registrerer "(.*?)" som aktiv låner/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+When(/^jeg registrerer utlån av "(.*?)"/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+Then(/^registrerer systemet at "(.*?)" er utlånt/) do
+	pending # express the regexp above with the code you wish you had
+end
+
+Then(/^at "(.*?)" låner "(.*?)"/) do
+	pending # express the regexp above with the code you wish you had
+end
