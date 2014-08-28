3b2a1cdfe1857d9be9b0cfadb000c7c77a50d5f7
Rurik Greenall <rurik.greenall@gmail.com>
2014-08-28 10:53:18 +0200

    Add test of MARC structure table
    Add test of MARC structure table, which indicates that the web installer has run and we have a MARC structure that can be used

--- salt/koha/files/updatekohadbversion.sh
+++ salt/koha/files/updatekohadbversion.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # small script to inject Koha db version to fresh install not done by webinstaller
 KOHAVERSION=`perl -e 'require "/usr/share/koha/intranet/cgi-bin/kohaversion.pl" ; print kohaversion();' 2> /dev/null`
+
+
 if [[ -n "$KOHAVERSION" ]] ; then
   KOHADBVERSION=`echo ${KOHAVERSION} | awk -F. '{ print $1"."$2$3$4 }'`
   KOHADBVERSIONOLD=`echo -n "SELECT value as '' FROM systempreferences WHERE variable = 'Version';" | sudo koha-mysql $INSTANCE | tail -1`
+  MARCTAGSTRUCTURE=`echo -n "SELECT COUNT(*) FROM koha_name.marc_tag_structure where tagfield = 008;" | sudo koha-mysql $INSTANCE | tail -1`
+
   #KOHADBVERSIONNEW = ''.join(kohaversion.rsplit('.', 2)) %}
 
-  if [[ ! -z $KOHADBVERSIONOLD && ${KOHADBVERSIONOLD+x} ]] && [[ $KOHADBVERSIONOLD == $KOHADBVERSION ]] ; then
+  if [[ ! -z $KOHADBVERSIONOLD && ${KOHADBVERSIONOLD+x} ]] && [[ $KOHADBVERSIONOLD = $KOHADBVERSION ]] && [[ $MARCTAGSTRUCTURE = "1" ]] ; then
     # Up to date!
     CMD=""
     CHANGED=no
-    COMMENT="koha DB ($KOHADBVERSION and KohaOld $KOHADBVERSIONOLD) version already up to date!"
+    COMMENT="Koha DB is up-to-date (version $KOHAVERSION) and MARC tag structure is nominally in place"
 
     # return Salt State line
     echo  # an empty line here so the next line will be the last.
