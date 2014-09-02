a4d473c68148ff647114ad7703961c4724d405bf
Benjamin Rokseth <bensinober@gmail.com>
2014-09-02 14:02:12 +0200

    Add scenario title to @context dump

--- test/features/step_definitions/patron_steps.rb
+++ test/features/step_definitions/patron_steps.rb
@@ -69,7 +69,7 @@ When(/^jeg legger inn "(.*?)" som ny låner$/) do |name|
 end
 
 Then(/^kan jeg se kategorien i listen over lånerkategorier$/) do
-  table = @browser.table(:id => "table_categorie").wait_until_present(timeout = 10)
+  table = @browser.table(:id => "table_categorie").wait_until_present
   table.should be_present
   table.text.should include(@context[:patron_category_code])
 end
--- test/features/support/hooks.rb
+++ test/features/support/hooks.rb
@@ -36,6 +36,7 @@ end
 #  AFTER HOOKS will run in the OPPOSITE order of which they are registered.
 
 After do #dump the context
+  @context[:featurename] = scenario.title
   PP.pp @context
 end
 
