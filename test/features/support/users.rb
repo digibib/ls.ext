c35b60da888e3baf2a8d4b83186c8ae3226cc25b
Benjamin Rokseth <bensinober@gmail.com>
2015-01-16 15:41:02 +0100

    Add method to import users via admin csv import

--- test/features/support/users.rb
+++ test/features/support/users.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 module Users
 
   def user_exists?(searchString)
@@ -29,4 +31,63 @@ module Users
       return false
     end
   end
+
+  # Method to import user hash via CSV in admin
+  # Branch and patron category are randomized
+  def import_user_via_csv(user)
+    user[:branchcode]   = @context[:branchcode]
+    user[:categorycode] = @context[:patron_category_code]
+    user[:cardnumber]   = generateRandomString
+    user[:surname]      = generateRandomString
+
+    # To convert to CSV via Migration 
+    importuser = { user[:cardnumber] => user }
+
+    uri = URI.parse intranet(:patron_import)
+
+    # Generate multipart form
+    form_boundary = generateRandomString
+    data = []
+    data << "--#{form_boundary}\r\n"
+    data << "Content-Disposition: form-data; name=\"uploadborrowers\"; filename=\"patrons.csv\"\r\n"
+    data << "Content-Type: text/csv\r\n"
+    data << "\r\n"
+    data << Migration.to_csv(importuser)
+    data << "--#{form_boundary}\r\n"
+    data << "Content-Disposition: form-data; name=\"matchpoint\"\r\n"
+    data << "\r\n"
+    data << "cardnumber\r\n"
+    data << "--#{form_boundary}\r\n"
+    data << "Content-Disposition: form-data; name=\"overwrite_cardnumber\"\r\n"
+    data << "\r\n"
+    data << "1\r\n"
+    data << "--#{form_boundary}--\r\n"
+
+    session_cookie = "CGISESSID=#{@browser.cookies["CGISESSID"][:value]}"
+    headers = {
+     "Cookie" => session_cookie,
+     "Content-Type" => "multipart/form-data, boundary=#{form_boundary}"
+    }
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.request_uri, headers)
+    req.body = data.join
+    res = http.request(req)
+
+    @context[:cardnumber] = user[:cardnumber]
+
+    @cleanup.push( "lånernummer #{@context[:cardnumber]}" =>
+      lambda do
+        @browser.goto intranet(:patrons)
+        @browser.text_field(:id => "searchmember").set @context[:cardnumber]
+        @browser.form(:action => "/cgi-bin/koha/members/member.pl").submit
+        @browser.div(:class => 'patroninfo').wait_until_present
+        #Phantomjs doesn't handle javascript popus, so we must override
+        #the confirm function to simulate "OK" click:
+        @browser.execute_script("window.confirm = function(msg){return true;}")
+        @browser.button(:text => "More").click
+        @browser.a(:id => "deletepatron").click
+      end
+    )
+  end
+
 end
