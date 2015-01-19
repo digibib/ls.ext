0ade66d31d3c6b56daf263564cd302a82f5e799b
Benjamin Rokseth <bensinober@gmail.com>
2015-01-19 16:25:10 +0100

    Removed autotmat steps from circulation steps

--- test/features/step_definitions/circulation_steps.rb
+++ test/features/step_definitions/circulation_steps.rb
@@ -1,58 +1,10 @@
 # encoding: UTF-8
 
 require 'pp'
-require_relative '../support/SIP2Client.rb'
 
 Given(/^at en bok er utlånt til en låner$/) do
   step "jeg registrerer \"Knut\" som aktiv låner"
   step "jeg registrerer utlån av boka"
-end
-
-#############################
-# AUTOMAT CIRCULATION STEPS
-#############################
-
-# Automat user should not be deleted after creation
-Given(/^at det finnes en utlånsautomat$/) do
-  step "at jeg er logget inn som adminbruker"
-  unless user_exists?("Automat")
-    # prereq: library
-    branchcode   = generateRandomString
-    branchname   = generateRandomString
-    categorycode = generateRandomString
-    categorydesc = generateRandomString
-
-    @browser.goto intranet(:branches)
-    @browser.link(:id => "newbranch").click
-    form = @browser.form(:name => "Aform")
-    form.text_field(:id => "branchname").set branchname
-    form.text_field(:id => "branchcode").set branchcode
-    form.submit
-
-    # prereq: patron category type
-    @browser.goto intranet(:patron_categories)
-    @browser.link(:id => "newcategory").click
-    form = @browser.form(:name => "Aform")
-    form.text_field(:id => "categorycode").set categorycode
-    form.text_field(:id => "description").set categorydesc
-    form.select_list(:id => "category_type").select "Staff"
-    form.text_field(:id => "enrolmentperiod").set "1"  # Months
-    form.submit
-
-    # user
-    @browser.goto intranet(:patrons)
-    @browser.button(:text => "New patron").click
-    @browser.div(:class => "btn-group").ul(:class => "dropdown-menu").a(:text => categorydesc).click
-    form = @browser.form(:name => "form")
-    form.text_field(:id => "firstname").set "Audun"
-    form.text_field(:id => "surname").set "Automat"
-    form.text_field(:id => "userid").set "autouser"
-    form.text_field(:id => "password").set "autopass"
-    form.text_field(:id => "password2").set "autopass"
-    form.button(:name => "save").click
-
-    step "at bruker \"Automat\" har rettighet \"circulate\""
-  end
 end
 
 Given(/^at det finnes materiale som er utlånt til låneren$/) do
@@ -62,51 +14,6 @@ end
 Given(/^at materialet har en eieravdeling$/) do
   pending # express the regexp above with the code you wish you had
 end
-
-When(/^låneren velger å låne på automaten$/) do
-  sip = SIP2Client.new("192.168.50.12", 6001)
-  res = sip.connect
-  res[:statusCode].should eq("Login_Response")
-  res[:statusData].should eq("1")
-
-  res = sip.status
-  res[:statusCode].should eq("ACS_Status")
-  res[:statusData].should include("YYYYNN100005")
-  res["BX"].should eq("YYYYYYYYYYYNYYYY")
-
-  @context[:sip_client] = sip
-  @cleanup.push( "SIP2 connection" =>
-    lambda do
-      @context[:sip_client].close
-    end
-  )
-end
-
-When(/^låneren identifiserer seg på automat med (riktig|feil) PIN$/) do | pin |
-  pin == "riktig" ? pin = @context[:password] : pin = "0000"
-  @context[:sip_patron_information] = @context[:sip_client].userlogin(@context[:branchcode],@context[:cardnumber],pin)
-  @context[:sip_patron_information]["AE"].should include("Knut #{@context[:surname]}")
-end
-
-Then(/^får låneren beskjed om at PIN( ikke)? er riktig$/) do |invalidpin|
-  validpin = invalidpin ? "N" : "Y"
-  @context[:sip_patron_information]["CQ"].should eq(validpin)
-end
-
-Then(/^får låneren beskjed om at lånekort( ikke)? er gyldig$/) do |invalidcard|
-  validcard = invalidcard ? "N" : "Y"
-  @context[:sip_patron_information]["BL"].should eq(validcard)
-end
-
-Then(/^får låneren beskjed om at lånekort er sperret$/) do
-  @context[:sip_patron_information][:statusData][0...4].should eq("YYYY")
-end
-
-Then(/^får låneren mulighet til å registrere lån på automaten$/) do
-  @context[:sip_patron_information]["AF"].should include("Greetings from Koha.")
-  @context[:sip_patron_information][:statusData][0...4].should_not include("Y") # 'Y' in any of these fields denote privileges denied
-end
-
 
 When(/^jeg registrerer "(.*?)" som aktiv låner$/) do |patron|
   @browser.goto intranet(:home)
@@ -236,5 +143,3 @@ Given(/^at sirkulasjonsreglene på sida stemmer overens med følgende data$/) do
   b.should == true
 
 end
-
-
