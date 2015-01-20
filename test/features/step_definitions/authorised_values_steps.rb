14a63fded5fceaa2999d5462eb2372f38388c465
Benjamin Rokseth <bensinober@gmail.com>
2015-01-20 18:52:44 +0100

    Authorised values steps cleanup, ref #3

--- test/features/step_definitions/authorised_values_steps.rb
+++ test/features/step_definitions/authorised_values_steps.rb
@@ -1,33 +1,33 @@
 # encoding: UTF-8
 
 Given(/^det finnes en autorisert verdi for "(.*)"$/) do |category|
-  @context[:authorised_category]    = category
-  @context[:authorised_value]       = generateRandomString
-  @context[:authorised_description] = generateRandomString
+  @context[:authorised_value_category]    = category
+  @context[:authorised_value]             = generateRandomString
+  @context[:authorised_value_description] = generateRandomString
   @browser.goto intranet(:authorised_values)
 
   form = @browser.form(:id => "category")
 
-  category_exists = form.select.include?(@context[:authorised_category])
+  category_exists = form.select.include?(@context[:authorised_value_category])
   
   if category_exists
-    form.select_list(:id => "searchfield").select_value @context[:authorised_category]
+    form.select_list(:id => "searchfield").select_value @context[:authorised_value_category]
     @browser.a(:id => "addauth").click
   else
     @browser.a(:id => "addcat").click
   end
 
   form = @browser.form(:name => "Aform")
-  form.text_field(:id => "category").set @context[:authorised_category]
+  form.text_field(:id => "category").set @context[:authorised_value_category]
   form.text_field(:id => "authorised_value").set @context[:authorised_value]
-  form.text_field(:id => "lib").set @context[:authorised_description]
+  form.text_field(:id => "lib").set @context[:authorised_value_description]
   form.submit
 
-  @cleanup.push( "autorisert verdi #{@context[:authorised_value]} for #{@context[:authorised_category]}" =>
+  @cleanup.push( "autorisert verdi #{@context[:authorised_value]} for #{@context[:authorised_value_category]}" =>
     lambda do
       @browser.goto intranet(:authorised_values)
       form = @browser.form(:id => "category")
-      form.select_list(:id => "searchfield").select_value @context[:authorised_category]
+      form.select_list(:id => "searchfield").select_value @context[:authorised_value_category]
 
       table = @browser.table(:id => "table_authorized_values")
       table.rows.each do |row|
@@ -48,7 +48,7 @@ end
 Then(/^kan jeg finne den autoriserte verdien i listen over autoriserte verdier$/) do
   @browser.goto intranet(:authorised_values)
   form = @browser.form(:id => "category")
-  form.select_list(:id => "searchfield").select_value @context[:authorised_category]
+  form.select_list(:id => "searchfield").select_value @context[:authorised_value_category]
   table = @browser.table(:id => "table_authorized_values")
   table.wait_until_present
   table.text.should include @context[:authorised_value]
