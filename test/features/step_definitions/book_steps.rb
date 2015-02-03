d2dd41fe300a442a36e3e44ee97ac1443c38477b
Benjamin Rokseth <bensinober@gmail.com>
2015-02-03 13:38:40 +0100

    Add SVC::Biblio service

--- test/features/step_definitions/book_steps.rb
+++ test/features/step_definitions/book_steps.rb
@@ -18,81 +18,13 @@ Given(/^at boka er tilgjengelig$/) do
 end
 
 When(/^jeg legger inn boka som en ny bok$/) do
-  @http = Net::HTTP.new(host, 8081)
-  res = @http.get("/cgi-bin/koha/svc/authentication?userid=#{SETTINGS['koha']['adminuser']}&password=#{SETTINGS['koha']['adminpass']}")
-  res.body.should_not include("failed")
-  @context[:svc_cookie] = res.response['set-cookie']
-
-  # Book item needs branch and itemtype before import
-  step "at det finnes en avdeling"       unless @active[:branch]
-  step "jeg legger til en materialtype"  unless @active[:itemtype]
-  book = Book.new
-  book.addItem
-  book.items.first.branch   = @active[:branch]
-  book.items.first.itemtype = @active[:itemtype]
-
-  data = File.read("features/upload-files/Fargelegg byen!.normarc", :encoding => 'UTF-8')
-  data = data.gsub(/\{\{ book_title \}\}/, book.title)
-  data = data.gsub(/\{\{ branchcode \}\}/, @active[:branch].code)
-  data = data.gsub(/\{\{ item_type_code \}\}/, @active[:itemtype].code)
-  data = data.gsub(/\{\{ item_barcode \}\}/, book.items.first.barcode)
-  headers = {
-    'Cookie' => @context[:svc_cookie],
-    'Content-Type' => 'text/xml'
-  }
-  # NB! param items=1 means imports items from $952
-  # Branch codes in $952a/b MUST exist as Libraries in Koha for items,
-  # barcode, etc to be imported!
-  res = @http.post("/cgi-bin/koha/svc/new_bib?items=1", data, headers)
-  res.body.should include("<status>ok</status>")
-  book.biblionumber = res.body.match(/<biblionumber>(\d+)<\/biblionumber>/)[1]
-
-  #barcode_search = res.body.match(/code=\"p\">(\d+)<\/subfield>/)
-  #STDOUT.puts "DEBUG PRINT res.body: #{res.body}" if !barcode_search || barcode_search.length < 2
-  #@book.items.first.barcode = barcode_search[1]
-  # force rebuild and restart zebra bibliographic index
-  `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-rebuild-zebra -f #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
-    # stop zebra
-  `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-stop-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
-    # start zebra
-  `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-start-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+  book = SVC::Biblio.new(@browser,@context,@active).add
 
   @active[:book] = book
   (@context[:books] ||= []) << book
   @cleanup.push( "bok #{book.biblionumber}" =>
     lambda do
-      @browser.goto intranet(:bib_record)+book.biblionumber
-
-      #delete book items
-      @browser.execute_script("window.confirm = function(msg){return true;}")
-      @browser.button(:text => "Edit").click
-      @browser.a(:id => "deleteallitems").click
-
-      #delete book record
-      @browser.execute_script("window.confirm = function(msg){return true;}")
-      @browser.button(:text => "Edit").click
-      @browser.a(:id => "deletebiblio").click
-
-      #delete item type
-      #@browser.goto intranet(:item_types)
-      #table = @browser.table(:id => "table_item_type")
-      #table.wait_until_present
-      #table.rows.each do |row|
-      #  if row.text.include?(@itemtype.code)
-      #    row.link(:href => /op=delete_confirm/).click
-      #    @browser.input(:value => "Delete this Item Type").click
-      #    break
-      #  end
-      #end
-
-      #delete branch
-      #@browser.goto intranet(:branches)
-      #@browser.div(:id => "branchest_filter").text_field().set(@branch.name)
-      #@browser.link(:href => "?branchcode=" + @branch.code + "&branchname=" + @branch.name + "&op=delete").click
-      #form = @browser.form(:action => "/cgi-bin/koha/admin/branches.pl")
-      #if form.text.include?(@branch.code)
-      #  form.submit
-      #end
+      SVC::Biblio.new(@browser).delete(book)
     end
   )
 end
---
+++ test/features/support/services/svc/biblio.rb
@@ -0,0 +1,67 @@
+# encoding: UTF-8
+
+require 'net/http'
+require 'uri'
+require 'json'
+require_relative '../service.rb'
+require_relative '../../context_structs.rb'
+
+module SVC
+  class Biblio < Service
+
+    def add
+      http = Net::HTTP.new(host, 8081)
+      res = http.get("/cgi-bin/koha/svc/authentication?userid=#{SETTINGS['koha']['adminuser']}&password=#{SETTINGS['koha']['adminpass']}")
+      res.body.should_not include("failed")
+      @context[:svc_cookie] = res.response['set-cookie']
+
+      # Book item needs branch and itemtype before import
+      step "at det finnes en avdeling"       unless @active[:branch]
+      step "jeg legger til en materialtype"  unless @active[:itemtype]
+      book = Book.new
+      book.addItem
+      book.items.first.branch   = @active[:branch]
+      book.items.first.itemtype = @active[:itemtype]
+
+      data = File.read("features/upload-files/Fargelegg byen!.normarc", :encoding => 'UTF-8')
+      data = data.gsub(/\{\{ book_title \}\}/, book.title)
+      data = data.gsub(/\{\{ branchcode \}\}/, @active[:branch].code)
+      data = data.gsub(/\{\{ item_type_code \}\}/, @active[:itemtype].code)
+      data = data.gsub(/\{\{ item_barcode \}\}/, book.items.first.barcode)
+      headers = {
+        'Cookie' => @context[:svc_cookie],
+        'Content-Type' => 'text/xml'
+      }
+      # NB! param items=1 means imports items from $952
+      # Branch codes in $952a/b MUST exist as Libraries in Koha for items,
+      # barcode, etc to be imported!
+      res = http.post("/cgi-bin/koha/svc/new_bib?items=1", data, headers)
+      res.body.should include("<status>ok</status>")
+      book.biblionumber = res.body.match(/<biblionumber>(\d+)<\/biblionumber>/)[1]
+
+      # force rebuild and restart zebra bibliographic index
+      `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-rebuild-zebra -f #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+        # stop zebra
+      `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-stop-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+        # start zebra
+      `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-start-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+
+      return book
+    end
+
+    def delete(book)
+      @browser.goto intranet(:bib_record)+book.biblionumber
+
+      #delete book items
+      @browser.execute_script("window.confirm = function(msg){return true;}")
+      @browser.button(:text => "Edit").click
+      @browser.a(:id => "deleteallitems").click
+
+      #delete book record
+      @browser.execute_script("window.confirm = function(msg){return true;}")
+      @browser.button(:text => "Edit").click
+      @browser.a(:id => "deletebiblio").click
+    end
+
+  end
+end
