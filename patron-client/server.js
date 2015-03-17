af7c7d702665664b8e50b435860b70b8d05cee4d
Stig B. Dørmænen <stigbd@gmail.com>
2015-03-17 12:58:34 +0100

    Introduce express
    Rename hello.js to server.js

---
+++ patron-client/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World, satan!')
+});
+
+var server = app.listen(8000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Server listening at http://%s:%s', host, port);
+
+});
