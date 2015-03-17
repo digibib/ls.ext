b00d6f646ca973491f2716754e874cb5f2c98490
Stig B. Dørmænen <stigbd@gmail.com>
2015-03-17 09:24:34 +0100

    Format code properly

--- services/src/main/java/no/deichman/services/Work.java
+++ services/src/main/java/no/deichman/services/Work.java
@@ -1,14 +1,14 @@
 package no.deichman.services;
- 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
- 
+
 @Path("/work")
 public class Work {
- 
+
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
