7ede36c02e2bcd0295c5d779d6db34268c3880ce
akafred <github@akafred.com>
2015-03-17 13:25:06 +0100

    Lint server.js.

--- patron-client/server.js
+++ patron-client/server.js
@@ -1,15 +1,18 @@
+/*jslint unparam: true, node: true */
+"use strict";
+
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World, satan!')
+    res.send('Hello World!');
 });
 
 var server = app.listen(8000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address,
+        port = server.address().port;
 
-  console.log('Server listening at http://%s:%s', host, port);
+    console.log('Server listening at http://%s:%s', host, port);
 
 });
