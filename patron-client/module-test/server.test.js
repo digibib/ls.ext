7ed03bece362b82b7ef942838a21bb6e231cd8a5
akafred <github@akafred.com>
2015-03-17 15:09:52 +0100

    Fix server test which didn't fail when it should.

--- patron-client/module-test/server.test.js
+++ patron-client/module-test/server.test.js
@@ -3,18 +3,20 @@
 "use strict";
 var assert = require("assert"),
     chai = require("chai"),
+    expect = chai.expect,
     chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
 describe('PatronClient', function () {
-    describe('#root()', function () {
-        it('should return hello world', function () {
+    describe('/', function () {
+        it('should return status OK', function (done) {
             chai.request('http://192.168.50.50:7000').
                 get('/').
                 end(function (err, res) {
-                    expect(err).to.be.null();
+                    expect(err).to.equal(null);
                     expect(res).to.have.status(200);
+                    done();
                 });
         });
     });
