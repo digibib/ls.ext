6c6719d15ff8e1caf348b52ecf5b6b904196914a
Rurik Greenall <rurik.greenall@gmail.com>
2015-03-18 12:39:11 +0100

    Add test for presence of title text in module html response

--- patron-client/module-test/server.test.js
+++ patron-client/module-test/server.test.js
@@ -4,20 +4,42 @@
 var assert = require("assert"),
     chai = require("chai"),
     expect = chai.expect,
-    chaiHttp = require('chai-http');
+    chaiHttp = require('chai-http'),
+    http = require('http'),
+    cheerio = require('cheerio');
 
 chai.use(chaiHttp);
+
+var parameters = {hostname: '192.168.50.50', port: '7000', path: '/'};
 
 describe('PatronClient', function () {
     describe('/', function () {
         it('should return status OK', function (done) {
-            chai.request('http://192.168.50.50:7000').
-                get('/').
+            chai.request('http://' + parameters.hostname + ':' + parameters.port).
+                get(parameters.path).
                 end(function (err, res) {
                     expect(err).to.equal(null);
                     expect(res).to.have.status(200);
                     done();
                 });
+        });
+    });
+    describe('/', function () {
+        it('should find text "Sult av Knut Hamsun"', function (done) {
+            http.get(parameters, function (res) {
+                var body = '',
+                    $ = '';
+                res.on('data', function (chunk) {
+                    body += chunk;
+                });
+                res.on('end', function () {
+                    $ = cheerio.load(body);
+                    expect($('h1').text()).to.equal("Sult av Knut Hamsun");
+                });
+                done();
+            }).on('error', function (e) {
+                console.log("Got error: ", e);
+            });
         });
     });
 });
