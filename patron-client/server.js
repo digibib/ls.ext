7849c119b7138891b144ea4b7bc2f00e95dbf120
Rurik Greenall <rurik.greenall@gmail.com>
2015-03-18 17:29:24 +0100

    Add templating and mock data, tidy up old mocks

--- patron-client/server.js
+++ patron-client/server.js
@@ -1,10 +1,20 @@
 /*jslint unparam: true, node: true */
 "use strict";
 
-var express = require('express');
+var express = require('express'),
+    hogan = require('fs-hogan').set({ templates: './templates' });
 var app = express();
 
-app.use(express.static('public'));
+//http://192.168.50.50:8080/work/1
+var data = {"Title": "Sult", "creator": "Knut Hamsun", "date": "1890", "editions": [{"id": "edition_00001", "isbn": "82-05-27748-6", "placement": "Voksenavdelingen, Hovedbiblioteket", "shelf": "magasinet", "status": "På hylla"}, {"id": "edition_00001", "isbn": "82-05-27748-6", "placement": "Voksenavdelingen, Hovedbiblioteket", "status": "På hylla"}]};
+
+hogan.renderFile('index.hjs', data, function (err, text) {
+    app.get('/', function (req, res) {
+        res.send(text);
+    });
+});
+
+
 
 var server = app.listen(8000, function () {
 
