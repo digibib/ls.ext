12a836ba0d5991aa9e51936bc6e9186eb59939e0
Rurik Greenall <rurik.greenall@gmail.com>
2015-03-19 01:46:36 +0100

    Fix and tidy server.js

--- patron-client/server.js
+++ patron-client/server.js
@@ -3,36 +3,30 @@
 
 var express = require('express'),
     http = require('http'),
-    hogan = require('fs-hogan').set({ templates: './templates' });
+    hogan = require('fs-hogan');
 var app = express();
 
-var parameters = {hostname: '192.168.50.50', port: '8080', path: '/work/1'};
+app.set('view engine', 'hjs');
+app.engine('hjs', hogan.renderFile);
 
-var render = function (data) {
-    hogan.renderFile('index.hjs', data, function (err, text) {
-        app.get('/', function (req, res) {
-            res.send(text);
+app.get('/', function (request, response) {
+    var parameters = {hostname: '192.168.50.50', port: '8080', path: '/work/1'};
+    http.get(parameters, function (res) {
+        var body = '';
+        res.on('data', function (chunk) {
+            body += chunk;
         });
+        res.on('end', function () {
+            var data = JSON.parse(body);
+            response.render('index', data);
+        });
+    }).on('error', function (e) {
+        console.log("Got error: ", e);
     });
-};
-
-http.get(parameters, function (res) {
-    var body = '';
-    res.on('data', function (chunk) {
-        body += chunk;
-    });
-    res.on('end', function () {
-        render(JSON.parse(body));
-    });
-}).on('error', function (e) {
-    console.log("Got error: ", e);
 });
 
 var server = app.listen(8000, function () {
-
     var host = server.address().address,
         port = server.address().port;
-
     console.log('Server listening at http://%s:%s', host, port);
-
 });
