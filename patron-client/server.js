c234462fa745a8e9704d6a5a8fa8ecc25db3f032
Stig B. Dørmænen <stigbd@gmail.com>
2015-03-25 14:14:25 +0100

    Add better error-handling in patron-client

--- patron-client/server.js
+++ patron-client/server.js
@@ -10,6 +10,16 @@ var app = express();
 app.set('view engine', 'hjs');
 app.engine('hjs', hogan.renderFile);
 
+function getData(body) {
+    var data = {};
+    try {
+        data = {ok: true, data: JSON.parse(body)};
+    } catch (error) {
+        data = {ok: false, data: error};
+    }
+    return data;
+}
+
 app.get('/work/:id', function (request, response) {
 
     var parameters = config.get();
@@ -21,9 +31,14 @@ app.get('/work/:id', function (request, response) {
             body += chunk;
         });
         res.on('end', function () {
-            var data = JSON.parse(body);
-            console.log("Received from services for '" + parameters.path + "': " + body);
-            response.render('index', data);
+            var data = getData(body);
+            if (data.ok) {
+                console.log("Received from services for '" + parameters.path + "': " + body);
+                response.render('index', data.data);
+            } else {
+                response.status("404");
+                response.render('error', data);
+            }
         });
     }).on('error', function (e) {
         console.log("Got error: ", e);
