digibib/ls.ext

commit 841ed9a43f9a2e2732f7422810f285891f11aa90

Use generated keys for ssh between virtualboxes.
Vagrant 1.7 has started using generated keys (not well known keys), so
we need to change our approach to make ssh from vm-test to vm-ship possible.

First time vagrant is run we generate a keypair. The keypair is given to the
vagrant user in vm-test and the public key is added to authorized_keys in
vm-ship.

Vagrantfile
@@ -24,6 +24,8 @@ if !File.file?(migration_pillar_file)
   raise "ERROR: You need to create a valid #{migration_pillar_file} based on #{migration_pillar_example_file}"
 end
 
+`ssh/generate_keys.sh`
+
 Vagrant.configure(2) do |config|
 
   # **** vm-ship - Docker container ship ****
@@ -48,6 +50,8 @@ Vagrant.configure(2) do |config|
     config.vm.synced_folder "pillar", "/srv/pillar"
 
     config.vm.provision "shell", path: "pip_install.sh"
+
+    config.vm.provision "shell", path: "ssh/accept_keys.sh"
 
     config.vm.provision :salt do |salt|
       salt.minion_config = "salt/minion"
@@ -88,12 +92,7 @@ Vagrant.configure(2) do |config|
 
     config.vm.network "private_network", ip: "192.168.50.11"
 
-    # get vagrant insecure private key to vm-test to allow ssh from vm-test to vm-ext
-    config.vm.provision "shell", inline: <<-SCRIPT
-      wget --no-check-certificate https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant -O /home/vagrant/.ssh/insecure_private_key
-      chmod 600 /home/vagrant/.ssh/insecure_private_key
-      chown vagrant:vagrant /home/vagrant/.ssh/insecure_private_key
-    SCRIPT
+    config.vm.provision "shell", path: "ssh/add_keys.sh"
 
     config.vm.provision :salt do |salt|
       salt.minion_config = "test/salt/minion"

ssh/generate_keys.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+
+# Vagrant 1.7 no longer operates with a well know keypair so we generate our own.
+KEYFILE=id_rsa
+
+if [ -e "ssh/$KEYFILE" ]
+then
+    echo "$KEYFILE exists"
+else
+    echo "generating key pair $KEYFILE"
+    ssh-keygen -C "ls.ext - ssh key pair" -t rsa -P "" -f ssh/$KEYFILE
+fi

.gitignore
@@ -1,4 +1,5 @@
 .Trash-1000
 .vagrant
 deploy.log
-
+ssh/id_rsa
+ssh/id_rsa.pub

ssh/accept_keys.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+
+KEYFILE=id_rsa
+
+grep "ls.ext" /home/vagrant/.ssh/authorized_keys  >/dev/null 2>&1
+
+if [ $? -eq 0 ]
+then
+    echo "ls.ext key found in authorized_keys"
+else
+    echo "adding ls.ext key to authorized_keys"
+    cat /vagrant/ssh/$KEYFILE.pub >> /home/vagrant/.ssh/authorized_keys
+fi

ssh/add_keys.sh
@@ -1,0 +1,14 @@
+#!/bin/bash
+
+KEYFILE=id_rsa
+
+if [ -e ".ssh/$KEYFILE" ]
+then
+    echo "$KEYFILE in place"
+else
+    echo "copying keyfile"
+    cp /vagrant/ssh/id_rsa.pub /home/vagrant/.ssh/
+    chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+    cp /vagrant/ssh/id_rsa /home/vagrant/.ssh/
+    chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+fi

test/features/support/services/svc/biblio.rb
@@ -38,11 +38,11 @@ module SVC
       book.biblionumber = res.body.match(/<biblionumber>(\d+)<\/biblionumber>/)[1]
 
       # force rebuild and restart zebra bibliographic index
-      `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-rebuild-zebra -f #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+      `ssh 192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-rebuild-zebra -f #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
         # stop zebra
-      `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-stop-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+      `ssh 192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-stop-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
         # start zebra
-      `ssh -i ~/.ssh/insecure_private_key vagrant@192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-start-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
+      `ssh 192.168.50.12 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 'sudo docker exec koha_container sudo koha-start-zebra #{SETTINGS['koha']['instance']}' > /dev/null 2>&1`
 
       return book
     end

commit 772939f3894105752db12bab057c8fa636e31d29

Fix for running vagrant commands in subdirectories.
The ssh key handling from commit 841ed9a broke the ability to run
vagrant commands from subdirectories.

Vagrantfile
@@ -24,7 +24,7 @@ if !File.file?(migration_pillar_file)
   raise "ERROR: You need to create a valid #{migration_pillar_file} based on #{migration_pillar_example_file}"
 end
 
-`ssh/generate_keys.sh`
+`VAGRANT_ROOT=#{vagrant_root} #{vagrant_root}/ssh/generate_keys.sh`
 
 Vagrant.configure(2) do |config|
 

ssh/generate_keys.sh
@@ -3,10 +3,10 @@
 # Vagrant 1.7 no longer operates with a well know keypair so we generate our own.
 KEYFILE=id_rsa
 
-if [ -e "ssh/$KEYFILE" ]
+if [ -e "$VAGRANT_ROOT/ssh/$KEYFILE" ]
 then
     echo "$KEYFILE exists"
 else
     echo "generating key pair $KEYFILE"
-    ssh-keygen -C "ls.ext - ssh key pair" -t rsa -P "" -f ssh/$KEYFILE
+    ssh-keygen -C "ls.ext - ssh key pair" -t rsa -P "" -f $VAGRANT_ROOT/ssh/$KEYFILE
 fi