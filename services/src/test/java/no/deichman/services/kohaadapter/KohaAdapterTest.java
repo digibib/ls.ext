2d9346bf2a9383f978b2dec638e54c9c2255e70d
Stig B. Dørmænen <stigbd@gmail.com>
2015-03-26 09:48:45 +0100

    Tidy up
    Remove unused imports
    Remove empty methods
    Remove unnecessary comments
    Remove unnecessary print to stdout

--- services/src/test/java/no/deichman/services/kohaadapter/KohaAdapterTest.java
+++ services/src/test/java/no/deichman/services/kohaadapter/KohaAdapterTest.java
@@ -1,11 +1,7 @@
 package no.deichman.services.kohaadapter;
 
 import com.hp.hpl.jena.rdf.model.Model;
-import org.junit.After;
-import org.junit.AfterClass;
 import static org.junit.Assert.assertNotNull;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -13,33 +9,9 @@ import org.junit.Test;
  * @author sbd
  */
 public class KohaAdapterTest {
-    
-    public KohaAdapterTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-
-    /**
-     * Test of getBiblio method, of class KohaAdapterDefault.
-     */
     @Test
     public void should_return_biblio() {
-        System.out.println("getBiblio");
         String manifestationId = "1";
         KohaAdapterMock instance = new KohaAdapterMock();
         Model expResult = null;
