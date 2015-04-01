40d36694b087a0d4102c367f64ac863553ab23b2
Rurik Greenall <rurik.greenall@gmail.com>
2015-04-01 10:41:33 +0200

    Add api for work data from user interface

---
+++ catalinker/lib/RESTService.rb
@@ -0,0 +1,39 @@
+require 'rest-client'
+require 'uri'
+require 'rdf'
+
+class RESTService
+
+  @@base = "http://deichman.no/work/"
+  @@dcterms = "http://purl.org/dc/terms/"
+  @@deichman = "http://deichman.no/vocab/"
+  @@type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+
+  def process_work (data)
+
+  	id = data[:id]
+  	author = data[:author]
+  	title = data[:title]
+  	date = data[:date]
+
+    s = RDF::URI.new(@@base + id)
+
+    rdfservice = RDFService.new
+
+    rdfservice.add_triple(s, RDF::URI.new(@@type), RDF::URI.new(@@base + "Work"))
+
+    if !author.empty?
+    	rdfservice.add_triple(s, RDF::URI.new(@@dcterms + "creator"), RDF::URI.new(author))
+    end
+    if !title.empty?
+    	rdfservice.add_triple(s, RDF::URI.new(@@dcterms + "title"), RDF::Literal.new(title[:string], :language => "#{title[:language]}"))
+    end
+    if !date.empty?
+    	rdfservice.add_triple(s, RDF::URI.new(@@dcterms + "date"), RDF::Literal.new(date[:string], :datatype => date[:datatype]))
+    end
+
+    return rdfservice.get_model
+
+  end
+
+end
---
+++ catalinker/tests/test_RESTService.rb
@@ -0,0 +1,48 @@
+ENV['RACK_ENV'] = 'test'
+require_relative '../lib/RESTService'
+require 'test/unit'
+require 'rack/test'
+require 'rdf'
+require 'rdf/turtle'
+require 'rdf/isomorphic'
+
+class RDFServiceTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def test_it_can_create_complete_work
+
+  	dcterms = RDF::Vocabulary.new("http://purl.org/dc/terms/")
+  	rdf = RDF::Vocabulary.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+
+    repo = RDF::Repository.new
+    test = repo.load('./tests/data/work_0001.nt')
+
+    query = RDF::Query.new({
+      :id => {
+        rdf.type  => :type,
+        dcterms.creator => :creator,
+        dcterms.title => :title,
+        dcterms.date => :date
+      }
+    })
+
+    data = nil
+    
+    query.execute(repo) do |solution|
+      data = {
+        :id => solution.id.to_s,
+        :author => solution.creator.to_s,
+        :title => {:string => solution.title, :language => solution.title.language},
+        :date => {:string => solution.date, :datatype => solution.date.datatype}
+      }
+    end
+
+    rs = RESTService.new
+    output = rs.process_work data
+
+    bijection = test.bijection_to output
+
+    assert(bijection == nil, "The round-tripped models were not the same: #{bijection}")
+  end
+
+end
