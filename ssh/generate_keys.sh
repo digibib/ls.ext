95beaabc183ff124afdaf01fc871793555dc0c03
akafred <github@akafred.com>
2015-04-08 14:56:49 +0200

    Avoid passing VAGRANT_ROOT into script.
    Seems to cause an issue with the Vagrantfile on some os/ruby-combos.

--- ssh/generate_keys.sh
+++ ssh/generate_keys.sh
@@ -3,12 +3,14 @@
 # Vagrant 1.7 no longer operates with a well know keypair so we generate our own.
 KEYFILE=id_rsa
 
-if [ -e "$VAGRANT_ROOT/ssh/$KEYFILE" ]
+DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+if [ -e "$DIR/$KEYFILE" ]
 then
-    KEY=`cat $VAGRANT_ROOT/ssh/$KEYFILE.pub`
+    KEY=`cat $DIR/$KEYFILE.pub`
     echo "$KEYFILE exists: $KEY"
 else
-    ssh-keygen -C "ls.ext - ssh key pair" -t rsa -P "" -f $VAGRANT_ROOT/ssh/$KEYFILE
-    KEY=`cat $VAGRANT_ROOT/ssh/$KEYFILE.pub`
+    ssh-keygen -C "ls.ext - ssh key pair" -t rsa -P "" -f $DIR/$KEYFILE
+    KEY=`cat $DIR/$KEYFILE.pub`
     echo "generated key pair $KEYFILE: $KEY"
 fi
