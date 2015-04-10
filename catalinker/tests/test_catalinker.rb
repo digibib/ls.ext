8ddb8fbb9dbfd8d113230dbf4943d6835fcb2d35
Rurik Greenall <rurik.greenall@gmail.com>
2015-04-10 16:04:14 +0200

    Revert previous change

--- catalinker/tests/test_catalinker.rb
+++ catalinker/tests/test_catalinker.rb
@@ -27,6 +27,10 @@ class ServerTest < Test::Unit::TestCase
   end
 
   def test_it_has_a_work_data_service
+    stub_request(:post, "http://192.168.50.50:8080/work").
+      with(:body => "{\n  \"@context\": {\n    \"dc\": \"http://purl.org/dc/terms/\"\n  },\n  \"@id\": \"http://deichman.no/work/work_THIS_EXISTS\",\n  \"@type\": \"http://deichman.no/ontology#Work\",\n  \"dc:creator\": {\n    \"@id\": \"http://example.com/person1\"\n  },\n  \"dc:date\": \"2009\",\n  \"dc:identifier\": \"work_THIS_EXISTS\",\n  \"dc:title\": \"This is a test\"\n}",
+      :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).
+      to_return(:status => 201, :body => "", :headers => {})
 
     post '/work', params = {:id => "work_THIS_EXISTS", :title => "This is a test", :creator => "http://example.com/person1", :date => "2009"}
     assert last_response.ok?
