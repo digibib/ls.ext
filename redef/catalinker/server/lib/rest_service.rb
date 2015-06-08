8d60d2134fc3170c86b3f3cfe7d6a537ed46b853
akafred <github@akafred.com>
2015-06-08 12:30:52 +0200

    Fix fallback ip.

--- redef/catalinker/server/lib/rest_service.rb
+++ redef/catalinker/server/lib/rest_service.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 
 class RESTService
 
-  SERVICES_PORT = (ENV['SERVICES_PORT'] || 'http://192.168.50.50:8080').sub(/^tcp:\//, 'http:/' )
+  SERVICES_PORT = (ENV['SERVICES_PORT'] || 'http://192.168.50.12:8080').sub(/^tcp:\//, 'http:/' )
 
   def self.push (resource, data)
     RestClient.post SERVICES_PORT + '/' + resource.to_s, data, :content_type => :json
