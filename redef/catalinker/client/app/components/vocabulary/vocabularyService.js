4f4013ad8a4270b68573fc5726252ab78399eb66
akafred <github@akafred.com>
2015-06-11 14:04:43 +0200

    Fix indent.

--- redef/catalinker/client/app/components/vocabulary/vocabularyService.js
+++ redef/catalinker/client/app/components/vocabulary/vocabularyService.js
@@ -6,74 +6,73 @@
             return $http.get(ontologyUri, { headers: { accept: 'application/ld+json' } });
         }])
         .factory('$vocabulary', ['$q', 'ontologyFactory', function($q, ontologyFactory) {
-        var context,
-            reverseContext = {},
-            vocab,
-            labelIndex = {},
-            deferred;
-        
-        function replaceUriWithContext(uri) {
-            var result = uri;
-            angular.forEach(context, function(c) {
-                if (uri.indexOf(c) === 0) {
-                    result = c;
-                }
+            var context,
+                reverseContext = {},
+                vocab,
+                labelIndex = {},
+                deferred;
+
+            function replaceUriWithContext(uri) {
+                var result = uri;
+                angular.forEach(context, function(c) {
+                    if (uri.indexOf(c) === 0) {
+                        result = c;
+                    }
+                });
+                return result;
+            }
+
+            function init(ontology) {
+                context = ontology['@context'];
+                vocab = ontology['@graph'];
+
+                angular.forEach(context, function(value, key) {
+                    reverseContext[value] = key;
+                });
+
+                vocab.forEach(function (obj) {
+                    var label = {
+                            'default': obj['rdfs:label']
+                        },
+                        comment = {
+                            'default': obj['rdfs:comment']
+                       };
+                    if (obj['rdfs:label'] instanceof Array) {
+                        obj['rdfs:label'].forEach(function(l) {
+                            label[l['@language']] = l['@value'];
+                        });
+                        label['default'] = label.no || label.en;
+                    }
+                    if (obj['rdfs:comment'] instanceof Array) {
+                        obj['rdfs:comment'].forEach(function (c) {
+                            comment[c['@language']] = c['@value'];
+                        });
+                        comment['default'] = comment.no || comment.en;
+                    }
+                    //obj['@label']
+                    labelIndex[obj['@id']] = label;
+                    labelIndex[replaceUriWithContext(obj['@id'])] = label;
+                });
+            }
+        /*
+            function getLabel(predicate) {
+                return labelIndex[predicate] || predicate;
+            }
+        */
+            deferred = $q.defer();
+            ontologyFactory
+            .success(function(data) {
+                init(data);
+                deferred.resolve();
+            }).error(function() {
+                deferred.reject();
             });
-            return result;
-        }
 
-        function init(ontology) {
-            context = ontology['@context'];
-            vocab = ontology['@graph'];
-
-            angular.forEach(context, function(value, key) {
-                reverseContext[value] = key;
-            });
-
-            vocab.forEach(function (obj) {
-                var label = {
-                        'default': obj['rdfs:label']
-                    },
-                    comment = {
-                        'default': obj['rdfs:comment']
-                   };
-                if (obj['rdfs:label'] instanceof Array) {
-                    obj['rdfs:label'].forEach(function(l) {
-                        label[l['@language']] = l['@value'];
-                    });
-                    label['default'] = label.no || label.en;
-                }
-                if (obj['rdfs:comment'] instanceof Array) {
-                    obj['rdfs:comment'].forEach(function (c) {
-                        comment[c['@language']] = c['@value'];
-                    });
-                    comment['default'] = comment.no || comment.en;
-                }
-                //obj['@label']
-                labelIndex[obj['@id']] = label;
-                labelIndex[replaceUriWithContext(obj['@id'])] = label;
-            });
-        }
-    /*
-        function getLabel(predicate) {
-            return labelIndex[predicate] || predicate;
-        }
-    */
-        deferred = $q.defer();
-        ontologyFactory
-        .success(function(data) {
-            init(data);
-            deferred.resolve();
-        }).error(function() {
-            deferred.reject();
-        });
-
-        return {
-            //getLabel: getLabel,
-            labels: labelIndex,
-            vocabulary: vocab,
-            promise: deferred.promise
-        };
-    }]);
-
+            return {
+                //getLabel: getLabel,
+                labels: labelIndex,
+                vocabulary: vocab,
+                promise: deferred.promise
+            };
+        }]);
 }());
