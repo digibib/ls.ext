d2ff2c1b601d1bba027ff356d57113dc48d16786
akafred <github@akafred.com>
2015-06-12 15:44:52 +0200

    Extract promise conversion.

--- redef/catalinker/client/app/components/vocabulary/vocabularyService.js
+++ redef/catalinker/client/app/components/vocabulary/vocabularyService.js
@@ -1,21 +1,20 @@
 ﻿(function() {
 
+    function asNormalPromise($q, httpPromise) {
+        var deferred = $q.defer();
+        httpPromise
+            .success(function (data) {
+                deferred.resolve(data);
+            }).error(function (data, status, headers) {
+                deferred.reject({status: status, headers: headers, data: data});
+            });
+        return deferred.promise;
+    }
+
     angular.module('catalinker.vocabulary', [])
         .constant('ontologyUri', 'http://192.168.50.12:8005/ontology')
         .factory('ontologyFactory', ['$q', '$http', 'ontologyUri', function($q, $http, ontologyUri) {
-            var httpPromise,
-                deferred;
-
-            // convert httpPromise to normal promise
-            deferred = $q.defer();
-            httpPromise = $http.get(ontologyUri, { headers: { accept: 'application/ld+json' } });
-            httpPromise
-                .success(function(data) {
-                    deferred.resolve(data);
-                }).error(function(data, status, headers) {
-                    deferred.reject({status: status, headers: headers, data: data});
-                });
-            return deferred.promise;
+            return asNormalPromise($q, $http.get(ontologyUri, {headers: {accept: 'application/ld+json'}}));
         }])
         .factory('vocabulary', ['$q', 'ontologyFactory', function($q, ontologyFactory) {
             var context,
