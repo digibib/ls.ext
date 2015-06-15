37b7ef3ee2442b92eca506ded2dd802b0a54f791
akafred <github@akafred.com>
2015-06-15 13:38:02 +0200

    Move function to reduce scope.

--- redef/catalinker/client/app/components/vocabulary/vocabularyService.js
+++ redef/catalinker/client/app/components/vocabulary/vocabularyService.js
@@ -1,19 +1,19 @@
 ﻿(function() {
 
-    function asNormalPromise($q, httpPromise) {
-        var deferred = $q.defer();
-        httpPromise
-            .success(function (data) {
-                deferred.resolve(data);
-            }).error(function (data, status, headers) {
-                deferred.reject({status: status, headers: headers, data: data});
-            });
-        return deferred.promise;
-    }
-
     angular.module('catalinker.vocabulary', [])
         .constant('ontologyUri', 'http://192.168.50.12:8005/ontology')
         .factory('ontologyFactory', ['$q', '$http', 'ontologyUri', function($q, $http, ontologyUri) {
+            function asNormalPromise($q, httpPromise) {
+                var deferred = $q.defer();
+                httpPromise
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    }).error(function (data, status, headers) {
+                        deferred.reject({status: status, headers: headers, data: data});
+                    });
+                return deferred.promise;
+            }
+
             return asNormalPromise($q, $http.get(ontologyUri, {headers: {accept: 'application/ld+json'}}));
         }])
         .factory('vocabulary', ['$q', 'ontologyFactory', function($q, ontologyFactory) {
