cf2645f3a892c48e23f17fe15f642b5edf538be3
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2015-06-18 10:47:14 +0200

    Remove serialVersionUID.
    Should be set automatically for us.

--- redef/services/src/main/java/no/deichman/services/error/PatchException.java
+++ redef/services/src/main/java/no/deichman/services/error/PatchException.java
@@ -1,11 +1,7 @@
 package no.deichman.services.error;
 
 public class PatchException extends Exception {
-
-    private static final long serialVersionUID = 1138511097608670080L;
-
     public PatchException(String message) {
         super(message);
     }
-
 }
--- redef/services/src/main/java/no/deichman/services/error/PatchParserException.java
+++ redef/services/src/main/java/no/deichman/services/error/PatchParserException.java
@@ -1,9 +1,6 @@
 package no.deichman.services.error;
 
 public class PatchParserException extends Exception {
-
-    private static final long serialVersionUID = -1227213666110309643L;
-
     public PatchParserException(String message){
         super(message);
     }
