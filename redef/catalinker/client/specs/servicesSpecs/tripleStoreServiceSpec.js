01854f619d804cced95595ae77426896c0a8e0de
jornare <jorn.are.hatlelid@kul.oslo.kommune.no>
2015-07-02 11:34:41 +0200

    catalinker specs

--- redef/catalinker/client/specs/servicesSpecs/tripleStoreServiceSpec.js
+++ redef/catalinker/client/specs/servicesSpecs/tripleStoreServiceSpec.js
@@ -1,16 +1,111 @@
-xdescribe("catalinker.triplestore", function () {
+describe("Given catalinker.triplestore is loaded", function () {
     'use strict';
-
+    var $httpBackend,
+        $injector,
+        $window,
+        $rootScope,
+        mocks = {};
+    
+    function loadMocks() {
+        angular.forEach($window.__html__, function (val, path) {
+            if (!path || !$window.__html__.hasOwnProperty(path)) {
+                return;
+            }
+            var key = path.substr(path.lastIndexOf('/') + 1);
+            key = key.substr(0, key.length - 5); //strip .json
+            mocks[key] = val;
+        });
+    }
+    
     beforeEach(function () {
+        module('catalinker.config');
         module('catalinker.triplestore');
     });
-
-
-    describe("Triple", function() {
-        // TODO
+    
+    beforeEach(inject(function (_$injector_) {
+        $injector = _$injector_;
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        $window = $injector.get('$window');
+        loadMocks();
+    }));
+    
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
 
-    describe("getDescription", function() {
+    describe("and triple store is loaded successfully", function () {
+        var $triplestore, isReady = false;
+        beforeEach(function() {
+            $triplestore = $injector.get('$tripleStore');
+            $httpBackend.expectGET('/config').respond(200, mocks.config);
+            $triplestore.ready.then(function (data) {
+                isReady = true;
+            });
+            $httpBackend.flush();
+        });
+        it("it is ready", function() {
+            expect(isReady).toBe(true);
+        });
+
+        describe("And we create a new work description successfully", function () {
+            var isError = false;
+            beforeEach(function () {
+                $httpBackend.expectPOST('http://192.168.50.12:8005/Work').respond(200, '',{Location:'http://192.168.50.12:8005/work/w222557057913'});
+                $httpBackend.expectGET('http://192.168.50.12:8005/work/w222557057913').respond(200, mocks.work_new);
+                $triplestore.newDescription('Work').then(function() {
+
+                }, function() {
+                    isError = true;
+                });
+                $httpBackend.flush();
+            });
+            it("it failed", function () {
+                expect(isError).toBe(false);
+            });
+        });
+
+        describe("And we create a new work description unsuccessfully", function () {
+            var isError = false;
+            beforeEach(function () {
+                $httpBackend.expectPOST('http://192.168.50.12:8005/Work').respond(500, '', { Location: 'http://192.168.50.12:8005/work/w222557057913' });
+                //$httpBackend.expectGET('http://192.168.50.12:8005/work/w222557057913').respond(200, mocks.work_new);
+                $triplestore.newDescription('Work').then(function () {
+
+                }, function () {
+                    isError = true;
+                });
+                $httpBackend.flush();
+            });
+            it("it failed", function () {
+                expect(isError).toBe(true);
+            });
+        });
+
+    });
+    
+    describe("and config failed to load", function () {
+        var $triplestore, isReady = false, isError = false;
+        beforeEach(function () {
+            $triplestore = $injector.get('$tripleStore');
+            $httpBackend.expectGET('/config').respond(500, '');
+            $triplestore.ready.then(function (data) {
+                isReady = true;
+            }, function() {
+                isError = true;
+            });
+            $httpBackend.flush();
+        });
+        it("it is not ready", function () {
+            expect(isReady).toBe(false);
+        });
+        it("it failed", function () {
+            expect(isError).toBe(true);
+        });
+    });    
+
+    xdescribe("getDescription", function() {
         var $rootScope,
             $httpBackend,
             work_w222557057913_Uri = 'http://192.168.50.12:8005/work/w222557057913',
--- redef/catalinker/client/specs/servicesSpecs/vocabularyServiceSpec.js
+++ redef/catalinker/client/specs/servicesSpecs/vocabularyServiceSpec.js
@@ -1,91 +1,128 @@
-xdescribe("catalinker.vocabulary is initalized", function () {
+describe("Given catalinker.vocabulary is initalized", function () {
     'use strict';
+    var mocks = {},
+        ontologyUri = '',
+        $injector, 
+        $vocabulary,
+        $httpBackend,
+        $window;
+    
+    function loadMocks() {
+        angular.forEach($window.__html__, function (val, path) {
+            if (!path || !$window.__html__.hasOwnProperty(path)) {
+                return;
+            }
+            var key = path.substr(path.lastIndexOf('/') + 1);
+            key = key.substr(0, key.length - 5); //strip .json
+            mocks[key] = val;
+        });
+    }       
 
     beforeEach(function () {
         module('catalinker.config');
+        module('catalinker.ontology');
         module('catalinker.vocabulary');
     });
+    
+    beforeEach(inject(function (_$injector_) {
+        $injector = _$injector_;
+        $httpBackend = $injector.get('$httpBackend');
+        $window = $injector.get('$window');
+        loadMocks();
+    }));
 
-    xdescribe("VocabularyService", function() {
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+   
 
-        describe("replaceContextInUri", function() {
 
-            it("should replace context in uri", function() {
-                var vocabulary,
-                    contextHash = {
-                    "something": "http://deichman.no/something#",
-                    "": "http://deichman.no/nothing#"
-                };
+    describe("and $ontology is loaded successfully", function () {
+        var vocabulary = null;
 
-                inject(function (_vocabulary_) {
-                    vocabulary = _vocabulary_;
-                });
-
-                expect(vocabulary.replaceContextInUri(contextHash, "something:something"))
-                    .toBe("http://deichman.no/something#something");
-
-                expect(vocabulary.replaceContextInUri(contextHash, ":anything"))
-                    .toBe("http://deichman.no/nothing#anything");
-
-                expect(vocabulary.replaceContextInUri(contextHash, "whatever:not_found"))
-                    .toBe("whatever:not_found");
+        beforeEach(function () {
+            $vocabulary = $injector.get('$vocabulary');
+            $httpBackend.expectGET('/config').respond(200, mocks.config);
+            $httpBackend.expectGET(ontologyUri).respond(200, mocks.ontologytest);
+            $vocabulary.ready.then(function (data) {
+                vocabulary = data;
             });
+            $httpBackend.flush();
+
         });
 
-        describe("labels", function () {
-            var minimalOntology = {
-                "@context": {
-                    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-                    "ls-ext": "http://deichman.no/ontology#"
-                },
-                "@graph": [
-                    {
-                        "@id": "http://deichman.no/ontology#Work",
-                        "rdfs:label": [
-                            {
-                                "@language": "en",
-                                "@value": "Work"
-                            },
-                            {
-                                "@language": "no",
-                                "@value": "Verk"
-                            }
-                        ]
-                    },
-                    {
-                        "@id": "ls-ext:name",
-                        "rdfs:label": "Name/navn"
-                    }
-                ]};
+        it("vocabulary is returned by ready promise", function() {
+            expect(vocabulary).not.toBe(null);
+        });
 
-            beforeEach(function () {
-                module(function ($provide) {
-                    $provide.factory('ontologyFactory', function ($q) {
-                        var dfd = $q.defer();
-                        dfd.resolve(minimalOntology);
-                        return dfd.promise;
-                    });
-                });
+        it("vocabulary contains a Work with fully qualified uri", function () {
+            expect(vocabulary["http://deichman.no/ontology#Work"]).toBeDefined();
+        });
+
+        it("the work contains a default label", function () {
+            expect(vocabulary["http://deichman.no/ontology#Work"]["default"]).toBeDefined();
+        });
+
+        it("the work contains an english label", function () {
+            expect(vocabulary["http://deichman.no/ontology#Work"]["en"]).toBeDefined();
+        });
+
+        it("the work contains a norwegian label", function () {
+            expect(vocabulary["http://deichman.no/ontology#Work"]["no"]).toBeDefined();
+        });
+
+        it("vocabulary contains a Person with fully qualified uri", function () {
+            expect(vocabulary["http://deichman.no/ontology#Person"]).toBeDefined();
+        });
+        
+        it("the Person contains a default label", function () {
+            expect(vocabulary["http://deichman.no/ontology#Person"]["default"]).toBeDefined();
+        });
+        
+        it("the Person does not contain an english label", function () {
+            expect(vocabulary["http://deichman.no/ontology#Person"]["en"]).not.toBeDefined();
+        });
+        
+        it("the Person does not contain a norwegian label", function () {
+            expect(vocabulary["http://deichman.no/ontology#Person"]["no"]).not.toBeDefined();
+        });
+
+        it("vocabulary contains a PersonEnglish with fully qualified uri", function () {
+            expect(vocabulary["http://deichman.no/ontology#PersonEnglish"]).toBeDefined();
+        });
+        
+        it("the Person contains a default label", function () {
+            expect(vocabulary["http://deichman.no/ontology#PersonEnglish"]["default"]).toBeDefined();
+        });
+        
+        it("the Person does contain an english label", function () {
+            expect(vocabulary["http://deichman.no/ontology#PersonEnglish"]["en"]).toBeDefined();
+        });
+        
+        it("the Person does not contain a norwegian label", function () {
+            expect(vocabulary["http://deichman.no/ontology#PersonEnglish"]["no"]).not.toBeDefined();
+        });
+    });
+
+    describe("and $ontology fails to load", function() {
+        var vocabulary = null;
+
+        beforeEach(function() {
+            $vocabulary = $injector.get('$vocabulary');
+            $httpBackend.expectGET('/config').respond(200, mocks.config);
+            $httpBackend.expectGET(ontologyUri).respond(500, '');
+            $vocabulary.ready.then(function(data) {
+                vocabulary = data;
+            }, function() {
+                vocabulary = false;
             });
+            $httpBackend.flush();
 
-            it("should convert ontology into list of labels", function (done) {
+        });
 
-                inject(function (vocabulary) {
-                    vocabulary.then(function(labels) {
-                        expect(labels['http://deichman.no/ontology#Work']['default']).toBe('Verk');
-                        expect(labels['http://deichman.no/ontology#name']['default']).toBe('Name/navn');
-                        expect(Object.keys(labels).length).toBe(2);
-                        done();
-                    }, function() {
-                        fail();
-                    });
-                });
-
-                inject(function ($rootScope) {
-                    $rootScope.$apply();
-                    $rootScope.$digest();
-                });
-            });
+        it("vocabulary is empty and error promise is run", function() {
+            expect(vocabulary).toBe(false);
         });
     });
 });
