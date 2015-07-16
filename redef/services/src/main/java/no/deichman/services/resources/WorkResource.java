946af7014a63b8e76515d2c6f7e3337349d2dfd6
Rurik Greenall <rurik.greenall@gmail.com>
2015-07-16 11:04:21 +0200

    Remove unused import

--- redef/services/src/main/java/no/deichman/services/resources/WorkResource.java
+++ redef/services/src/main/java/no/deichman/services/resources/WorkResource.java
@@ -23,7 +23,6 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
