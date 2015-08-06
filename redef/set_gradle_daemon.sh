1e1aafb566f10dd3d4d26c665e2018c7aa20a1cb
akafred <github@akafred.com>
2015-08-06 20:28:01 +0200

    Fix in set_gradle_daemon on first run.

--- redef/set_gradle_daemon.sh
+++ redef/set_gradle_daemon.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Rewriting /home/vagrant/.gradle/gradle.properties with org.gradle.daemon=true"
+mkdir -p /home/vagrant/.gradle
 touch /home/vagrant/.gradle/gradle.properties && echo "org.gradle.daemon=true" > /home/vagrant/.gradle/gradle.properties
