67a9b6342095524676097c335d8bd3a3705c8885
akafred <github@akafred.com>
2015-08-07 14:58:28 +0200

    Make sure vagrant user owns .gradle

--- redef/set_gradle_daemon.sh
+++ redef/set_gradle_daemon.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 echo "Rewriting /home/vagrant/.gradle/gradle.properties with org.gradle.daemon=true"
 mkdir -p /home/vagrant/.gradle
-touch /home/vagrant/.gradle/gradle.properties && echo "org.gradle.daemon=true" > /home/vagrant/.gradle/gradle.properties
+chown vagrant:vagrant /home/vagrant/.gradle
+touch /home/vagrant/.gradle/gradle.properties
+echo "org.gradle.daemon=true" > /home/vagrant/.gradle/gradle.properties
+chown vagrant:vagrant /home/vagrant/.gradle/gradle.properties
