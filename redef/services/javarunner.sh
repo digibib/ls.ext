f6723cca2b2bf4a2aae35c378f63e0e637171879
akafred <github@akafred.com>
2015-09-24 13:40:41 +0200

    Properly propagate kills to java process.
    Inpired by https://github.com/gchudnov/docker-tools/blob/master/signals/background/program.sh

---
+++ redef/services/javarunner.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+pid=0
+
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+
+java $* &
+pid="$!"
+
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
