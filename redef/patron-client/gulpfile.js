f9aad506ede422db8ea31a66d6521ccc072698be
Nicho <nicholas.paulik@gmail.com>
2015-10-08 13:20:56 +0200

    patron-client: Fix gulpfile, run node server.js instead of browser-sync

--- redef/patron-client/gulpfile.js
+++ redef/patron-client/gulpfile.js
@@ -8,12 +8,11 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var del = require('del');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var exec = require('child_process').exec;
+var child = require('child_process');
+var fs = require('fs');
+
 
 gulp.task('javascript', function () {
-    // set up the browserify instance on a task basis
     var b = browserify({
         entries: ['src/search.js'],
         debug: true
@@ -23,29 +22,18 @@ gulp.task('javascript', function () {
         .pipe(source('bundle.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here.
         .pipe(uglify())
         .on('error', gutil.log)
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('public/js'))
 });
 
-/* If livereloading does not work, make sure you access the page via the correct address (e.g. localhost instead of 127.0.0.1), because of same-origin policy. */
 gulp.task('serve', function() {
-    browserSync({
-        server: {
-            baseDir: 'public'
-        },
-        notify: false,
-        open: false,
-        port: 8000,
-        ui: false,
-        host: 'redef_patron_client_skeleton_container'
-    });
-
-    gulp.watch(['*.html', 'js/*.js'], {cwd: 'public'}, reload);
+    var server = child.spawn('node', ['server.js']);
+    var log = fs.createWriteStream('server.log', {flags: 'a'});
+    server.stdout.pipe(log);
+    server.stderr.pipe(log);
 });
-
 
 gulp.task('html', function () {
     gulp.src('src/*.html')
@@ -59,7 +47,7 @@ gulp.task('images', function () {
 
 gulp.task('clean', function () {
     return del([
-        'public/*',
+        'public/*'
     ]);
 });
 
