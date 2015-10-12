b7a79dc813893d30e258111b9352fb9a06519c5a
Kristoffer Moe <kristoffer@kodemaker.no>
2015-10-12 12:27:22 +0200

    Fixed query that retrieves year and name from work berfore indexing

--- redef/services/src/main/java/no/deichman/services/search/SearchServiceImpl.java
+++ redef/services/src/main/java/no/deichman/services/search/SearchServiceImpl.java
@@ -1,5 +1,6 @@
 package no.deichman.services.search;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import no.deichman.services.uridefaults.BaseURI;
 import org.apache.commons.io.IOUtils;
@@ -25,8 +26,8 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.Response;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -50,54 +51,53 @@ public class SearchServiceImpl implements SearchService {
 
     @Override
     public final void indexWorkModel(Model workModel) {
-
         QueryExecution queryExecution = QueryExecutionFactory.create(QueryFactory.create(
                 "PREFIX  deichman: <" + BaseURI.remote().ontology() + "> "
-                        + "SELECT  * "
-                        + "WHERE { ?workUri deichman:name ?name }"), workModel);
-        ResultSet publicationSet = queryExecution.execSelect();
-        String encodedWorkId="";
+                        + "SELECT  ?workUri ?name ?year "
+                        + "WHERE { ?workUri <deichman:name> ?name ; \n <deichman:year> ?year . }"), workModel);
+        ResultSet workSet = queryExecution.execSelect();
 
-        while (publicationSet.hasNext()) {
-            QuerySolution solution =  publicationSet.next();
+        if (workSet.hasNext()) {
+            QuerySolution solution = workSet.next();
             RDFNode nameNode = solution.get("name");
             RDFNode uriNode = solution.get("workUri");
-            encodedWorkId = uriNode.toString();
-        }
-
-
-        Map<String, String> jsonMap = new HashMap<>();
-        String newJsonContent = new Gson().toJson(jsonMap);
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpPut httpPut = new HttpPut(workSearchUriBuilder.setPath("/search/work/" + encodedWorkId).build());
-            httpPut.setEntity(new StringEntity(newJsonContent, Charset.forName(UTF_8)));
-            try (CloseableHttpResponse putResponse = httpclient.execute(httpPut)) {
-                LOG.debug(putResponse.getStatusLine().toString());
+            RDFNode yearNode = solution.get("year");
+            String workUri = uriNode.toString();
+            Map<String, String> jsonMap = ImmutableMap.of(
+                    "@id", workUri,
+                    "name", nameNode.asLiteral().getString(),
+                    "year", yearNode.asLiteral().getString());
+            String newJsonContent = new Gson().toJson(jsonMap);
+            try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+                HttpPut httpPut = new HttpPut(workSearchUriBuilder.setPath("/search/work/" + URLEncoder.encode(workUri, UTF_8)).build());
+                httpPut.setEntity(new StringEntity(newJsonContent, Charset.forName(UTF_8)));
+                try (CloseableHttpResponse putResponse = httpclient.execute(httpPut)) {
+                    LOG.debug(putResponse.getStatusLine().toString());
+                }
+            } catch (Exception e) {
+                LOG.error(String.format("Failed to index %s in elasticsearch", workUri), e);
+                throw new ServerErrorException(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
             }
-        }  catch (Exception e) {
-            LOG.error(String.format("Failed to index %s in elasticsearch", encodedWorkId), e);
-            throw new ServerErrorException(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
         }
-
     }
 
     @Override
     public final Response search(String query) {
-            try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-                HttpGet httpget = new HttpGet(workSearchUriBuilder.setParameter("q", query).build());
-                try (CloseableHttpResponse response = httpclient.execute(httpget)) {
-                    HttpEntity responseEntity = response.getEntity();
-                    String jsonContent = IOUtils.toString(responseEntity.getContent());
-                    Response.ResponseBuilder responseBuilder = Response.ok(jsonContent);
-                    Header[] headers = response.getAllHeaders();
-                    for (Header header : headers) {
-                        responseBuilder = responseBuilder.header(header.getName(), header.getValue());
-                    }
-                    return responseBuilder.build();
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpGet httpget = new HttpGet(workSearchUriBuilder.setParameter("q", query).build());
+            try (CloseableHttpResponse response = httpclient.execute(httpget)) {
+                HttpEntity responseEntity = response.getEntity();
+                String jsonContent = IOUtils.toString(responseEntity.getContent());
+                Response.ResponseBuilder responseBuilder = Response.ok(jsonContent);
+                Header[] headers = response.getAllHeaders();
+                for (Header header : headers) {
+                    responseBuilder = responseBuilder.header(header.getName(), header.getValue());
                 }
-            } catch (Exception e) {
-                LOG.error(e.getMessage(), e);
-                throw new ServerErrorException(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+                return responseBuilder.build();
             }
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            throw new ServerErrorException(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+        }
     }
 }
