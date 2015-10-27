e4d0f3c6bfb5b5653f26a2d16224e526e5ce7b8f
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2015-10-27 13:28:48 +0100

    Add done to test, to handle async failure scenarios

--- redef/patron-client/test/config.test.js
+++ redef/patron-client/test/config.test.js
@@ -6,9 +6,10 @@ var expect = require('chai').expect,
 
 describe('config', function () {
   describe('#get(env)', function () {
-    it('should return server and port based on environment setting', function () {
+    it('should return server and port based on environment setting', function (done) {
       var env = { SERVICES_PORT: "http://someserver:666" };
       expect(config.get(env)).to.deep.equal({host: "someserver", port: "666"});
+      done();
     });
   });
 });
