7eb0ab43e018849186d6b83b00ed6e818be9cb0d
Benjamin Rokseth <bensinober@gmail.com>
2015-10-29 15:18:16 +0100

    Catalinker client: Remove requirejs, casperjs and phantomjs

--- redef/catalinker/client/src/stringutil.js
+++ redef/catalinker/client/src/stringutil.js
@@ -1,10 +1,5 @@
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define([], function () {
-      return (root.StringUtil = factory());
-    });
-  } else if (typeof module === 'object' && module.exports) {
+  if (typeof module === 'object' && module.exports) {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
