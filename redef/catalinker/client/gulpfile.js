b98239a788d329e94ff441f77926edec420fdd08
Benjamin Rokseth <bensinober@gmail.com>
2015-10-29 15:35:54 +0100

    Catalinker client: add gulpfile.js

---
+++ redef/catalinker/client/gulpfile.js
@@ -0,0 +1,53 @@
+/*global require*/
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var del = require('del');
+
+gulp.task('browserify', ['clean', 'images', 'html'], function () {
+  "use strict";
+  var b = browserify({
+    entries: ['src/ontology.js', 'src/graph.js', 'src/stringutil.js'],
+    debug: true
+  });
+  return b
+  .require('./src/ontology.js', {expose: "Ontology"})
+  .require('./src/graph.js', {expose: "Graph"})
+  .require('./src/stringutil.js', {expose: "StringUtil"})
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({loadMaps: true}))
+  .pipe(uglify())
+  .on('error', gutil.log)
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('public/js/'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('src/*.html')
+      .pipe(gulp.dest('public'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('src/*.png')
+      .pipe(gulp.dest('public'));
+});
+
+gulp.task('clean', function () {
+  return del([
+      'public/js/*.js', 'public/*.html'
+  ]);
+});
+
+gulp.task('watch', ['clean','browserify'], function () {
+  gulp.watch(['src/*.html'], ['html']);
+  gulp.watch(['src/*.js'], ['browserify']);
+});
+
+gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['clean', 'images', 'html', 'browserify']);
