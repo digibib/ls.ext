c706b7fe5c70979fa6e1af5610fe030c6ffa6bbe
Benjamin Rokseth <bensinober@gmail.com>
2015-10-30 16:19:50 +0100

    Catalinker client: update module-tests to jsdom

--- redef/catalinker/client/module-test/work_test.js
+++ redef/catalinker/client/module-test/work_test.js
@@ -18,12 +18,19 @@ describe("Catalinker", function () {
       // load module
       Main = require("../src/main.js");
 
-      // stub ID returned from window.location
-      //sinon.stub(Work, "getResourceID", function () {
-      //  return "w123456";
-      //});
+      /* STUBS */
 
-      // stub http requests from axios used in module, faking returned promises
+      // ID returned from window.location
+      sinon.stub(Main, "getResourceType", function () {
+        return "Work";
+      });
+
+      // URI parameter
+      sinon.stub(Main, "getURLParameter", function () {
+        return "http://192.168.50.12:7000/work/w123456";
+      });
+
+      // http requests from axios used in module, faking returned promises
       sinon.stub(axios, "get", function (path) {
 
         switch (path) {
@@ -31,49 +38,95 @@ describe("Catalinker", function () {
           return Promise.resolve({data: {
             kohaOpacUri: "http://koha.deichman.no",
             kohaIntraUri: "http://koha.deichman.no",
-            ontologyUri: "http://127.0.0.1:7777/ontology",
-            resourceApiUri: "http://127.0.0.1:7777/"
+            ontologyUri: "http://192.168.50.12:7000/ontology",
+            resourceApiUri: "http://192.168.50.12:7000/"
           }});
-        case "/ontology":
-          return Promise.resolve({data: fs.readFileSync(__dirname + "mocks/ontology.json", "UTF-8") });
-        case "/authorized_values/language":
-          return Promise.resolve({data: fs.readFileSync(__dirname + "mocks/authorized_language.json", "UTF-8") });
-        case "/authorized_values/format":
-          return Promise.resolve({data: fs.readFileSync(__dirname + "mocks/authorized_format.json", "UTF-8") });
-        case "/index.html":
-          return Promise.resolve({data: fs.readFileSync(__dirname + "/../../src/index.html", "UTF-8") });
+        case "/index_template.html":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/../src/index_template.html", "UTF-8") });
+        case "http://192.168.50.12:7000/ontology":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/ontology.json", "UTF-8") });
+        case "http://192.168.50.12:7000/authorized_values/language":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/authorized_language.json", "UTF-8") });
+        case "http://192.168.50.12:7000/authorized_values/format":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/authorized_format.json", "UTF-8") });
         case "http://192.168.50.12:7000/work/w123456":
-          return Promise.resolve({data: fs.readFileSync(__dirname + "/../module-test/w123456.json", "UTF-8") });
-        case "http://192.168.50.12:7000/work/w123456/items":
-          return Promise.resolve({data: fs.readFileSync(__dirname + "/../module-test/w123456items.json", "UTF-8") });
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/w123456.json", "UTF-8") });
         }
+      });
+      // Stub creating new resource
+      sinon.stub(axios, "post", function (path, data, headers) {
+        return Promise.resolve({data: {}, headers: { location: ""} });
+      });
+
+      // Stub updating resource
+      sinon.stub(axios, "patch", function (path, data, headers) {
+        return Promise.resolve({data: {}, headers: { location: ""} });
       });
 
       // create fixtures for testing Ractive events
       var fixture = document.createElement("div");
-      var template = document.createElement("script");
       fixture.setAttribute("id", "container");
-      template.setAttribute("id", "template");
       document.body.appendChild(fixture);
-      document.body.appendChild(template);
 
       // load module
       Main.init().then(function (m) {
         ractive = m;
+        return Main.loadOntology();
+      }).then(function () {
         done();
       }).catch(function (err) {
         done(err);
       });
     });
 
-    after(function () {
+    after(function (done) {
       axios.get.restore();
+      axios.post.restore();
+      axios.patch.restore();
+      Main.getResourceType.restore();
+      Main.getURLParameter.restore();
+      done();
     });
 
-    it("Katalogisering av verk - har riktig tittel", function () {
-      expect("title").to.eq("Katalogisering av verk");
+    it('henter ressurstype fra siste ledd i pathname i url', function (done) {
+      expect(Main.getResourceType()).to.equal("Work");
+      done();
+    });
+
+    it("har riktig side-tittel", function (done) {
       expect(document.querySelector("h2[data-automation-id='page-heading']").innerHTML).to.equal("Katalogisering av verk");
+      done();
     });
 
+    it("gir riktig ressurs-URI", function (done) {
+      expect(document.querySelectorAll('[data-automation-id="resource_uri"]')[0].value).to.equal("http://192.168.50.12:7000/work/w123456");
+      done();
+    });
+
+    /*
+    it("gir riktig ressurs-ID, og er ikke redigerbar", function (done) {
+      expect(document.querySelectorAll('[data-automation-id="http://127.0.0.1:7777/ontology#recordID_0"]')[0].value).to.equal("123456");
+      expect(document.querySelectorAll('[data-automation-id="http://127.0.0.1:7777/ontology#recordID_0"]')[0].disabled).to.equal(true);
+      done();
+    });
+
+    it("gir riktig antall options i authorized values dropdown", function (done) {
+      expect(document.querySelectorAll('[class="prop-input http://127.0.0.1:7777/ontology#format"] option').length).to.equal(5);
+      expect(document.querySelectorAll('[class="prop-input http://127.0.0.1:7777/ontology#language"] option').length).to.equal(4);
+      done();
+    });
+
+    it("gir riktig options i format select list", function (done) {
+      var opts = document.querySelectorAll('[data-automation-id="http://127.0.0.1:7777/ontology#format_0"] select';
+      expect(opts).to.equal("something");
+      done();
+    });
+
+    it("gir riktig options i language select list", function (done) {
+      var opts = document.querySelectorAll('[data-automation-id="http://127.0.0.1:7777/ontology#language_0"] select';
+      expect(opts).to.equal("something");
+      done();
+    });
+    */
   });
 });
