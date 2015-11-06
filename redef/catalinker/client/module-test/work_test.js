07e7537658fdb9ea98f7e33c7efb029ff040dba3
Benjamin Rokseth <bensinober@gmail.com>
2015-11-06 16:27:21 +0100

    Catalinker: add module tests for person search feature

--- redef/catalinker/client/module-test/work_test.js
+++ redef/catalinker/client/module-test/work_test.js
@@ -46,6 +46,10 @@ describe("Catalinker", function () {
           return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/ontology.json", "UTF-8") });
         case "http://192.168.50.12:7000/work/w123456":
           return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/w123456.json", "UTF-8") });
+        case "http://192.168.50.12:7000/search/person/_search/?q=Monkey":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/person_search_hit.json", "UTF-8") });
+        case "http://192.168.50.12:7000/person/h123456":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/h123456.json", "UTF-8") });
         }
       });
       // Stub creating new resource
@@ -96,6 +100,44 @@ describe("Catalinker", function () {
     it("gir riktig ressurs-URI", function (done) {
       expect(document.querySelectorAll('[data-automation-id="resource_uri"]')[0].value).to.equal("http://192.168.50.12:7000/work/w123456");
       done();
+    });
+
+    describe("Søk på ressurser", function () {
+      it("søkefelt søker med riktig type og søkestreng", function (done) {
+        // searchResource event takes predicate and searchstring as parameters
+        testRactive.fire("searchResource", {}, "http://192.168.50.12:7000/ontology#creator", "Monkey");
+        testRactive.update().then(function () {           // force DOM update
+          var res = testRactive.get("search_result").results;
+          expect(res.hits.total).to.equal(2);
+          expect(res.hits.hits[0]._source.person.uri).to.equal("http://192.168.50.12:7000/person/h123456");
+          expect(res.hits.hits[0]._source.person.name).to.equal("Test Monkey");
+          expect(res.hits.hits[1]._source.person.uri).to.equal("http://192.168.50.12:7000/person/h234567");
+          expect(res.hits.hits[1]._source.person.name).to.equal("Another Test Monkey");
+          done();
+        }).catch(done);
+      });
+
+      it("presenterer søketreff i en liste", function (done) {
+        var res = fs.readFileSync(__dirname + "/mocks/person_search_hit.json", "UTF-8");
+        testRactive.set("search_result", {results: JSON.parse(res)}).then(function () {
+          expect(document.querySelectorAll(".search-result").length).to.equal(2);
+          expect(document.querySelectorAll(".search-result-name")[0].innerHTML).to.equal("Test Monkey");
+          expect(document.querySelectorAll(".search-result-info")[0].innerHTML).to.equal("(1944-1988)");
+          done();
+        }).catch(done);
+      });
+
+      it("oppdaterer riktig inputfelt med URI fra valgt søketreff", function (done) {
+        var origin = "inputs.1.values.0"; // keypath for originating search field
+        // selectResource event takes uri from context, and predicate and origin as parameters
+        testRactive.fire("selectResource", {context: {uri: "http://192.168.50.12:7000/work/h123456" }},
+          "http://192.168.50.12:7000/ontology#creator", origin);
+        testRactive.update().then(function () {
+          var creator = document.querySelectorAll('[data-automation-id="http://192.168.50.12:7000/ontology#creator_0"]')[0].value;
+          expect(creator).to.equal("http://192.168.50.12:7000/work/h123456");
+          done();
+        }).catch(done);
+      });
     });
   });
 });
