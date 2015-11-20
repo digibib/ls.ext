2f8fb1964245ffa2fd73e882bc3a05c3c7b68972
Benjamin Rokseth <bensinober@gmail.com>
2015-11-20 01:44:02 +0100

    #16 catalinker: add person test

---
+++ redef/catalinker/module-test/person_test.js
@@ -0,0 +1,123 @@
+/*global require, it, describe, before, after, document, Promise*/
+"use strict";
+
+var chai = require("chai"),
+  expect = chai.expect,
+  sinon = require("sinon"),
+  axios = require("axios"),
+  fs = require("fs"),
+  jsdom = require("mocha-jsdom");
+
+describe("Catalinker", function () {
+  describe("/Person", function () {
+    jsdom();
+    var testRactive, Main;
+
+    before(function (done) {
+
+      // load module
+      Main = require("../client/src/main.js");
+
+      // STUBS
+
+      sinon.stub(Main, "getResourceType", function () {
+        return "Person"; // ID returned from window.location
+      });
+
+      // URL parameter
+      sinon.stub(Main, "getURLParameter", function () {
+        return "http://192.168.50.12:7000/person/h123456";
+      });
+
+      // http requests from axios used in module, faking returned promises
+      sinon.stub(axios, "get", function (path) {
+
+        switch (path) {
+        case "/config":
+          return Promise.resolve({data: {
+            kohaOpacUri: "http://koha.deichman.no",
+            kohaIntraUri: "http://koha.deichman.no",
+            ontologyUri: "http://192.168.50.12:7000/ontology",
+            resourceApiUri: "http://192.168.50.12:7000/"
+          }});
+        case "/main_template.html":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/../public/main_template.html", "UTF-8") });
+        case "http://192.168.50.12:7000/ontology":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/ontology.json", "UTF-8") });
+        case "http://192.168.50.12:7000/authorized_values/nationality":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/authorized_nationality.json", "UTF-8") });
+        case "http://192.168.50.12:7000/person/h123456":
+          return Promise.resolve({data: fs.readFileSync(__dirname + "/mocks/h123456.json", "UTF-8") });
+        }
+      });
+      // Stub creating new resource
+      sinon.stub(axios, "post", function (path, data, headers) {
+        return Promise.resolve({data: {}, headers: { location: ""} });
+      });
+
+      // Stub updating resource
+      sinon.stub(axios, "patch", function (path, data, headers) {
+        return Promise.resolve({data: {}, headers: { location: ""} });
+      });
+
+      // create fixtures for testing Ractive events
+      var fixture = document.createElement("div");
+      fixture.setAttribute("id", "container");
+      document.body.appendChild(fixture);
+
+      // load module
+      Main.init().then(function (m) {
+        testRactive = m;
+        return Main.loadOntology();
+      }).then(function () {
+        return testRactive.update();
+      }).then(function () {
+        done();
+      }).catch(function (err) {
+        done(err);
+      });
+    });
+
+    after(function (done) {
+      axios.get.restore();
+      axios.post.restore();
+      axios.patch.restore();
+      Main.getResourceType.restore();
+      Main.getURLParameter.restore();
+      done();
+    });
+
+    describe("Registrere verk", function () {
+      it('henter ressurstype fra siste ledd i pathname i url', function (done) {
+        expect(Main.getResourceType()).to.equal("Person");
+        done();
+      });
+
+      it("har riktig side-tittel", function (done) {
+        expect(document.querySelector("h2[data-automation-id='page-heading']").innerHTML).to.equal("Katalogisering av person");
+        done();
+      });
+
+      it("gir riktig ressurs-URI", function (done) {
+        expect(document.querySelectorAll('[data-automation-id="resource_uri"]')[0].value).to.equal("http://192.168.50.12:7000/person/h123456");
+        done();
+      });
+    });
+
+    describe("Eksisterende person", function () {
+      it("populerer felt riktig", function (done) {
+        expect(document.querySelectorAll('[data-automation-id="http://192.168.50.12:7000/ontology#personTitle_0"]')[0].value).to.equal("Dr. Macabre");
+        expect(document.querySelectorAll('[data-automation-id="http://192.168.50.12:7000/ontology#name_0"]')[0].value).to.equal("R. Rolfsen");
+        expect(document.querySelectorAll('[data-automation-id="http://192.168.50.12:7000/ontology#birth_0"]')[0].value).to.equal("1977");
+        expect(document.querySelectorAll('[data-automation-id="http://192.168.50.12:7000/ontology#death_0"]')[0].value).to.equal("1981");
+        done();
+      });
+
+      it("gir riktig antall options i authorized values dropdown", function (done) {
+        expect(document.querySelectorAll('select[data-automation-id="http://192.168.50.12:7000/ontology#nationality_0"] option').length).to.equal(4);
+        done();
+      });
+    });
+
+  });
+});
