f147a55c55e8b65d363c57cca4a31bfbdaed8a7d
Benjamin Rokseth <bensinober@gmail.com>
2015-12-30 11:30:42 +0100

    Add docker-compose.env.example and explanation in docker-compose.sh

--- docker-compose/docker-compose.sh
+++ docker-compose/docker-compose.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This shell script replaces variables in docker-compose.yml with ENV vars
+# including (vars sourced from file docker-compose.env)
 cd /vagrant/docker-compose
 rm -rf docker-compose.yml
 if [ ! -f docker-compose.env ]; then
