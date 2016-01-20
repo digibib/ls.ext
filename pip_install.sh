11793c2b6155413395c0ce4c2ecf629b17badcab
Benjamin Rokseth <bensinober@gmail.com>
2016-01-20 11:31:00 +0100

    Pin pip version to 7.1.2 and make sure ssl deps are installed

--- pip_install.sh
+++ pip_install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
+PIP_VERSION=7.1.2
 dpkg -l python-pip >/dev/null 2>&1
 if [ $? -eq 0 ] ; then
 	echo "python-pip installed, removing ...";
 	sudo apt-get purge --assume-yes --quiet python-pip
 fi
 
-pip -V >/dev/null 2>&1
-if [ $? -eq 0 ] ; then
-    echo "pip already installed and presumably working."
-else
-	echo "Installing pip using easy install ...";
-	sudo apt-get install --assume-yes --quiet python-setuptools
-	sudo easy_install --script-dir=/usr/bin --upgrade pip
-    echo "pip installed."
-fi
+echo "Making sure pip is correct version: $PIP_VERSION";
+sudo apt-get install --assume-yes --quiet libffi-dev libssl-dev python-setuptools
+sudo easy_install --script-dir=/usr/bin --upgrade pip==$PIP_VERSION
