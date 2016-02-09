2a53ab1fa5332c2e22dc19752595b7cd8fd08123
Benjamin Rokseth <bensinober@gmail.com>
2016-02-09 15:34:36 +0100

    Add temporary fix to docker-in-docker issue, and pin docker-engine and docker-compose

--- docker_install.sh
+++ docker_install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-dpkg -l docker-engine >/dev/null 2>&1
-if [ $? -eq 0 ] ; then
-  echo "docker installed, removing ...";
-  sudo apt-get purge --assume-yes --quiet docker-engine
-fi
-
-sudo docker version --format '{{.Server.Version}}' >/dev/null 2>&1
-if [ $? -eq 0 ] ; then
-    echo "docker already installed and presumably working."
+VERSION=docker-engine=1.10.0-0~trusty
+INSTALLED=`dpkg -l | grep docker-engine | awk '{print $3}'`
+if [ $VERSION = $INSTALLED ] ; then
+  echo "docker version $VERSION already installed, skipping ...";
 else
-  echo "Installing docker...";
+  echo "Installing docker version $VERSION ...";
+  sudo apt-get purge --assume-yes --quiet docker-engine >/dev/null 2>&1
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get update
-  sudo apt-get -y install linux-image-extra-$(uname -r) make git docker-engine
+  sudo apt-get -y install linux-image-extra-$(uname -r) make git $VERSION
   echo "docker installed."
 fi
