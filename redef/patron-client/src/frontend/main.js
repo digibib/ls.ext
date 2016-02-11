56e74d588a9da8c3358dc914aa33096888f72e9b
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-02-11 09:55:01 +0100

    Patron-client: fix lint violations

--- redef/patron-client/src/frontend/main.js
+++ redef/patron-client/src/frontend/main.js
@@ -7,7 +7,7 @@ import { browserHistory } from 'react-router'
 import thunkMiddleware from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
 import { syncHistory } from 'react-router-redux'
-import createLogger from 'redux-logger'
+// import createLogger from 'redux-logger'
 
 import App from './containers/App'
 import Search from './containers/Search'
@@ -16,10 +16,10 @@ import Person from './containers/Person'
 import rootReducer from './reducers'
 
 const reduxRouterMiddleware = syncHistory(browserHistory)
-const loggerMiddleware = createLogger()
+// const loggerMiddleware = createLogger()
 const createStoreWithMiddleware = applyMiddleware(
   thunkMiddleware,
-  //loggerMiddleware,
+  // loggerMiddleware,
   reduxRouterMiddleware
 )(createStore)
 const store = createStoreWithMiddleware(rootReducer)
