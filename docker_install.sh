0644d2893fbaf569060afacc3229b656dcdb9f92
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-02-12 09:20:15 +0100

    Fix docker install

--- docker_install.sh
+++ docker_install.sh
@@ -9,6 +9,6 @@ else
   sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get update
-  sudo apt-get -y install linux-image-extra-$(uname -r) make git $VERSION
+  sudo apt-get -y install linux-image-extra-$(uname -r) make git docker-engine=$VERSION
   echo "docker installed."
 fi
