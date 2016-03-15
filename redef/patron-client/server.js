99acc38504e5a39fdea59b8456d8bee7be56128d
NichoP <nicho@computas.com>
2016-03-15 14:31:16 +0100

    patron-client: ES2015-ify server.js (still using require to avoid transpiler)

--- redef/patron-client/server.js
+++ redef/patron-client/server.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const path = require('path')
+const requestProxy = require('express-request-proxy');
 const port = process.env.PORT || 8000
 const app = express()
-var requestProxy = require('express-request-proxy');
 
 app.use(require('connect-livereload')())
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(`${__dirname}/public`))
 
 app.all('/services/*', requestProxy({
   url: 'http://services:8005/*'
 }))
 
-app.get('*', function (request, response) {
+app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 app.listen(port)
-console.log('Server started on port ' + port)
-
+console.log(`Server started on port ${port}`)
