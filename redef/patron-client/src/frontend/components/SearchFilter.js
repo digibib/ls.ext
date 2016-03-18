12d41f4e05fb7b0db295e18cb1be20159daede29
NichoP <nicho@computas.com>
2016-03-18 16:05:11 +0100

    patron-client: Fix after rebase

--- redef/patron-client/src/frontend/components/SearchFilter.js
+++ redef/patron-client/src/frontend/components/SearchFilter.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react'
 import Constants from '../constants/Constants'
+import { defineMessages, FormattedMessage } from 'react-intl'
 
 export default React.createClass({
   propTypes: {
