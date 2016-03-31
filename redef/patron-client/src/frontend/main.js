80c0b2c5afa57e5709d0e4f28ed0f167a85e7e4a
NichoP <nicho@computas.com>
2016-03-31 09:31:22 +0200

    patron-client: Add polyfill for Safari compatibility

--- redef/patron-client/src/frontend/main.js
+++ redef/patron-client/src/frontend/main.js
@@ -9,6 +9,10 @@ import { addLocaleData } from 'react-intl'
 import en from 'react-intl/locale-data/en'
 import no from 'react-intl/locale-data/no'
 
+if (!global.Intl) {
+  require('intl') // Safari requires this polyfill
+}
+
 addLocaleData(en)
 addLocaleData(no)
 
