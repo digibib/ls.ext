905538cc23ab4b9ab60114e12ec2e18cf16f9c52
NichoP <nicho@computas.com>
2016-04-11 15:01:28 +0200

    patron-client: Revert server.js, remove index.html

--- redef/patron-client/src/backend/server.js
+++ redef/patron-client/src/backend/server.js
@@ -5,17 +5,20 @@ const requestProxy = require('express-request-proxy')
 const port = process.env.PORT || 8000
 const app = express()
 
-const webpack = require('webpack')
-const webpackConfig = require('../../webpack.config')
-const compiler = webpack(webpackConfig)
-app.use(require('webpack-dev-middleware')(compiler, {
-  watchOptions: {
-    poll: process.env.POLL || false
-  },
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath
-}))
-app.use(require('webpack-hot-middleware')(compiler))
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackConfig = require('../../webpack.config')
+  const compiler = webpack(webpackConfig)
+  app.use(require('webpack-dev-middleware')(compiler, {
+    watchOptions: {
+      poll: process.env.POLL || false
+    },
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }))
+  app.use(require('webpack-hot-middleware')(compiler))
+  console.log('HMR activated')
+}
 
 app.use(express.static(`${__dirname}/../../public`))
 
