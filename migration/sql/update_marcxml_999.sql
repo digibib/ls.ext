00ddbfe5163674c49cd67f25fcd640572c5b6f63
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-07-25 07:25:05 +0200

    migration: wrap SQL query in transaction

--- migration/sql/update_marcxml_999.sql
+++ migration/sql/update_marcxml_999.sql
@@ -1,5 +1,7 @@
+START TRANSACTION;
 UPDATE biblioitems
 SET marcxml = UpdateXML(marcxml, '//datafield[@tag="999"]/subfield[@code="c"]', CONCAT('<subfield code="c">', biblioitemnumber, '</subfield>'));
 
 UPDATE biblioitems
 SET marcxml = UpdateXML(marcxml, '//datafield[@tag="999"]/subfield[@code="d"]', CONCAT('<subfield code="d">', biblioitemnumber, '</subfield>'));
+COMMIT;
