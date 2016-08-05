560ca03e3c38f5ab64f23c546c2d6f6de6152188
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-08-05 08:18:26 +0200

    migration: update sysprefs
    Some additions.
    Don't migrate syspref options, leave Koha defaults.

--- migration/sql/sysprefs.sql
+++ migration/sql/sysprefs.sql
@@ -4,7 +4,6 @@ LOCK TABLES `systempreferences` WRITE;
 CREATE TEMPORARY TABLE temptable (
   variable varchar(50) NOT NULL,
   value text,
-  options mediumtext,
   PRIMARY KEY (variable)
 ) ENGINE = MyISAM;
 
@@ -12,12 +11,12 @@ LOAD DATA LOCAL INFILE '/out/sysprefs.csv'
 INTO TABLE temptable
 CHARACTER SET utf8
 FIELDS TERMINATED BY ','
-OPTIONALLY ENCLOSED BY '\"'
+OPTIONALLY ENCLOSED BY '\''
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(variable, value, options);
+(variable, value);
 
-UPDATE systempreferences JOIN temptable USING (variable) SET systempreferences.value = temptable.value, systempreferences.options = temptable.options;
+UPDATE systempreferences JOIN temptable USING (variable) SET systempreferences.value = temptable.value;
 DROP TEMPORARY TABLE temptable;
 
 /*!40000 ALTER TABLE `systempreferences` ENABLE KEYS */;
