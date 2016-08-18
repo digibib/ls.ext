44050fd8d2e2886c07bcb53fd227a5dd39ecfc8c
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-08-18 08:26:46 +0200

    patron-client: don't use arrow function syntax in shared code
    Somehow, babel does not seem to transpile code shared by
    backend and frontend.

--- redef/patron-client/src/common/validation/validators.js
+++ redef/patron-client/src/common/validation/validators.js
@@ -1,30 +1,30 @@
 module.exports = {
-  email: (email) => {
+  email: function(email) {
     if (!/^[^@ ]+@[^@ ]+$/i.test(email)) {
       return 'invalidEmail'
     }
   },
-  year: (year) => {
+  year: function(year) {
     if (year < 1900 || year > new Date().getFullYear() - 5) { /* What is the actual requirements for age? */
       return 'invalidYear'
     }
   },
-  month: (month) => {
+  month: function(month) {
     if (month < 1 || month > 12) {
       return 'invalidMonth'
     }
   },
-  day: (day) => {
+  day: function(day) {
     if (day < 1 || day > 31) {
       return 'invalidDay'
     }
   },
-  pin: (pin) => {
+  pin: function(pin) {
     if (!/^\d{4}$/.test(pin)) {
       return 'invalidPin'
     }
   },
-  repeatPin: (repeatPin, values) => {
+  repeatPin: function(repeatPin, values) {
     if (!values.pin) {
       return
     }
