aa847cd4eb77740e530596b5a41913da2a752203
Benjamin Rokseth <bensinober@gmail.com>
2016-08-26 14:47:14 +0200

    DEICH-132: Add support for patron attribute door access
    Upgrade Koha to add SIP2 patron attribute validation in patron information request
    Add authorised values to migration
    Populate authorised values and patron attributes.

---
+++ migration/sql/avalues.sql
@@ -0,0 +1,21 @@
+
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Script to load data from csv into table `authorised_values`
+--
+
+LOCK TABLES `authorised_values` WRITE;
+DELETE FROM authorised_values WHERE category IN ("WITHDRAWN", "LOST", "NOT_LOAN", "RESTRICTED", "DAMAGED");
+/*!40000 ALTER TABLE `authorised_values` DISABLE KEYS */;
+
+LOAD DATA LOCAL INFILE '/out/avalues.csv'
+INTO TABLE authorised_values
+CHARACTER SET utf8
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(category, authorised_value, lib);
+
+/*!40000 ALTER TABLE `authorised_values` ENABLE KEYS */;
+UNLOCK TABLES;
--- migration/sql/setup_extended_patron_attributes.sql
+++ migration/sql/setup_extended_patron_attributes.sql
@@ -1,4 +1,5 @@
 INSERT IGNORE INTO borrower_attribute_types
-  (code, description, unique_id, staff_searchable)
+  (code, description, unique_id, staff_searchable, authorised_value_category)
 VALUES
-  ('fnr', 'Fødselsnummer', 1, 1);
+  ('fnr', 'Fødselsnummer', 1, 1, NULL),
+  ('dooraccess', 'Meråpent tilgang', 0, 0, 'DOORACCESS');
