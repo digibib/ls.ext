4981cf5d49f7b8102f41a13eaa310cfd714a509a
Benjamin Rokseth <bensinober@gmail.com>
2016-09-07 11:24:15 +0200

    DEICH-183: add classes to patron attributes

--- migration/sql/setup_extended_patron_attributes.sql
+++ migration/sql/setup_extended_patron_attributes.sql
@@ -1,5 +1,5 @@
 INSERT IGNORE INTO borrower_attribute_types
-  (code, description, unique_id, staff_searchable, authorised_value_category)
+  (code, description, unique_id, staff_searchable, authorised_value_category, class)
 VALUES
-  ('fnr', 'Fødselsnummer', 1, 1, NULL),
-  ('dooraccess', 'Meråpent tilgang', 0, 0, 'DOORACCESS');
+  ('fnr', 'Fødselsnummer', 1, 1, NULL, 'fnr'),
+  ('dooraccess', 'Meråpent tilgang', 0, 0, 'DOORACCESS', 'dooraccess');
