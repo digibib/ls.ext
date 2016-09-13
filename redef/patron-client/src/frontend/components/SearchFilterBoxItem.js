e5cf162b1e2fa853071889a50361c4e8d9f4f77d
NichoP <nicho@computas.com>
2016-09-13 14:28:29 +0200

    patron-client: SearchFilterBoxItem filter and linting

--- redef/patron-client/src/frontend/components/SearchFilterBoxItem.js
+++ redef/patron-client/src/frontend/components/SearchFilterBoxItem.js
@@ -1,25 +1,24 @@
-/* eslint-disable */
-import React, {PropTypes} from 'react'
-import {injectIntl, intlShape} from 'react-intl'
+import React, { PropTypes } from 'react'
+import { injectIntl, intlShape } from 'react-intl'
 import ClickableElement from '../components/ClickableElement'
 
-const SearchFilterBoxItem = ({filter, toggleFilter, intl}) => {
-    return (
-        <ClickableElement onClickAction={toggleFilter} onClickArguments={[filter.id]}>
-            <li className="active-filter" data-automation-id={filter.id}>
-                <span className="filter-label" data-automation-id='filter_label'>
-                    {intl.formatMessage({id: filter.bucket})}{/* ({filter.count}) */}
+const SearchFilterBoxItem = ({ filter, toggleFilter, intl }) => {
+  return (
+    <ClickableElement onClickAction={toggleFilter} onClickArguments={[ filter.id ]}>
+      <li className="active-filter" data-automation-id={filter.id}>
+                <span className="filter-label" data-automation-id="filter_label">
+                    {intl.formatMessage({ id: filter.bucket })}{/* ({filter.count}) */}
                 </span>
-                <span className="remove">X</span>
-            </li>
-        </ClickableElement>
-    )
+        <span className="remove">X</span>
+      </li>
+    </ClickableElement>
+  )
 }
 
 SearchFilterBoxItem.propTypes = {
-    filter: PropTypes.object.isRequired,
-    toggleFilter: PropTypes.func.isRequired,
-    intl: intlShape.isRequired
+  filter: PropTypes.object.isRequired,
+  toggleFilter: PropTypes.func.isRequired,
+  intl: intlShape.isRequired
 }
 
 export default injectIntl(SearchFilterBoxItem)
