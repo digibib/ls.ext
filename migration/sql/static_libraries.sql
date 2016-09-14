302fa0d36567e105624f123609d20ef310cee24e
Benjamin Rokseth <bensinober@gmail.com>
2016-09-15 00:02:06 +0200

    migration: add static categories,libraries and users

---
+++ migration/sql/static_libraries.sql
@@ -0,0 +1,23 @@
+INSERT IGNORE INTO branches
+  (branchcode, branchname)
+VALUES
+  ("api", "Internt API"),
+  ("ftor","Torshov"),
+  ("fbol","Bøler"),
+  ("ffur","Furuset"),
+  ("froa","Røa"),
+  ("fsme","Smestad"),
+  ("flam","Lambertseter"),
+  ("hutl","Hovedbiblioteket"),
+  ("fgry","Grünerløkka"),
+  ("fopp","Oppsal"),
+  ("fbje","Bjerke"),
+  ("frmm","Rommen"),
+  ("fhol","Holmlia"),
+  ("fnor","Nordtvet"),
+  ("fnyd","Nydalen"),
+  ("fbjo","Bjørnholt"),
+  ("from","Romsås"),
+  ("fgam","Gamle Oslo"),
+  ("fsto","Stovner"),
+  ("fmaj","Majorstua");
