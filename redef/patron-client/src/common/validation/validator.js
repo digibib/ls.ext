d31c54da2101b24dfea703a3d845e83a56d7c30f
entity <nikolai.larsen@soprasteria.com>
2016-09-16 14:35:52 +0200

    removed console.log

--- redef/patron-client/src/common/validation/validator.js
+++ redef/patron-client/src/common/validation/validator.js
@@ -1,17 +1,13 @@
 const validators = require('./validators')
 
 module.exports = form => values => {
-  console.log('validatorValues', form, values)
   const requiredFields = Object.keys(form).filter(field => {
-    console.log('required', field, form[ field ].required)
     return form[ field ].required
   })
   const errors = {}
-  console.log('required', values)
   Object.keys(form).forEach(field => {
     const value = values[ field ]
     if (requiredFields.includes(field) && !value) {
-      console.log('err required', field)
       errors[ field ] = 'required'
     } else if (!value) {
       return
