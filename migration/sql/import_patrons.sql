deaf4c0d8d29523c97b187c50e695e459a4fa7aa
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-09-29 14:17:00 +0200

    migration: borrowers.altcontactsurname

--- migration/sql/import_patrons.sql
+++ migration/sql/import_patrons.sql
@@ -4,5 +4,5 @@ CHARACTER SET utf8
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '\"'
 LINES TERMINATED BY '\n'
-(userid,cardnumber,surname,firstname,address,address2,zipcode,city,country,phone,smsalertnumber,email,categorycode,privacy,branchcode,sex,password,dateofbirth);
+(userid,cardnumber,surname,firstname,address,address2,zipcode,city,country,phone,smsalertnumber,email,categorycode,privacy,branchcode,sex,password,dateofbirth,altcontactsurname);
 SHOW WARNINGS;
