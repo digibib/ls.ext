63d4324a610d1b643c4c0583ec98ff46d557cfcd
Thomas H. Wiberg <thomas.wiberg@soprasteria.com>
2016-10-06 13:28:18 +0200

    DEICH-301 Patron client - Change footer info, add link

--- redef/patron-client/src/frontend/components/Footer.js
+++ redef/patron-client/src/frontend/components/Footer.js
@@ -14,31 +14,58 @@ class Footer extends React.Component {
 
   render () {
     return (
-      <ReactCSSTransitionGroup
-        transitionName="fade-in"
-        transitionAppear
-        transitionAppearTimeout={500}
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}
-        component="footer"
-        className="main-footer">
-        <nav className="secondary-navigation" type="navigation">
-          <ul>
-            <li><FormattedMessage {...messages.contactUs} /></li>
-            <li><FormattedMessage {...messages.openingHours} /></li>
-            <li data-automation-id="change_language_element"
-                data-current-language={this.props.intl.formatMessage(messages.currentLanguage)}
-                onClick={this.handleChangeLanguage}><FormattedMessage {...messages.languageChoice} /></li>
-            <li><FormattedMessage {...messages.aboutUs} /></li>
-          </ul>
-        </nav>
-        <div className="footer-text">
-          <p><FormattedHTMLMessage {...messages.info} /></p>
-        </div>
-        <div className="footer-icon">
-          <img src="/images/footer-icon.svg" alt="White box with black circle" />
-        </div>
-      </ReactCSSTransitionGroup>
+      <footer className="main-footer">
+        <ReactCSSTransitionGroup
+          transitionName="fade-in"
+          transitionAppear
+          transitionAppearTimeout={500}
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+          component="div"
+          className="footer-content">
+          <nav className="secondary-navigation" type="navigation">
+            <ul>
+              <li>
+                <a href="https://www.deichman.no/kontakt-oss">
+                  <FormattedMessage {...messages.contactUs} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.deichman.no/filialer">
+                  <FormattedMessage {...messages.openingHours} />
+                </a>
+              </li>
+              <li data-automation-id="change_language_element"
+                  data-current-language={this.props.intl.formatMessage(messages.currentLanguage)}
+                  onClick={this.handleChangeLanguage}><FormattedMessage {...messages.languageChoice} /></li>
+              <li>
+                <a href="https://www.deichman.no/om-oss">
+                  <FormattedMessage {...messages.aboutUs} />
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="footer-text">
+            <FormattedHTMLMessage {...messages.info} />
+          </div>
+
+          <div className="footer-right">
+
+            <div className="feedback">
+              <a href="https://goo.gl/forms/QvwgiEhD2rdsWZLv1" target="_blank">
+                <FormattedMessage {...messages.feedback} />
+              </a>
+            </div>
+
+            <div className="footer-icon">
+              <img src="/images/footer-icon.svg" alt="White box with black circle" />
+            </div>
+
+          </div>
+
+        </ReactCSSTransitionGroup>
+      </footer>
     )
   }
 }
@@ -79,6 +106,11 @@ export const messages = defineMessages({
     id: 'Footer.info',
     description: 'The text containing the address, telephone number and other information',
     defaultMessage: 'Deichmanske bibliotek - Oslo public library <br />Oslo kommune <br />Arne Garborgs plass 4, 0179 Oslo, Norway <br />Telephone: +47 23 43 29 00 <br />Editor: Knut Skansen (acting Director) <br />'
+  },
+  feedback: {
+    id: 'Footer.feedback',
+    description: 'Feeback description',
+    defaultMessage: 'Innspill på søk eller Min Side?'
   }
 })
 
