0c8efbd6a8efa7513159457070e0729e16845c1a
NichoP <nicho@computas.com>
2016-10-14 10:32:21 +0200

    patron-client: Fix showing borrower name, fix linting

--- redef/patron-client/src/backend/routes/auth.js
+++ redef/patron-client/src/backend/routes/auth.js
@@ -13,22 +13,22 @@ module.exports = (app) => {
         if (res.status === 200) {
           return res.json()
         } else {
-          return Promise.reject({message: 'User not found', status: 403})
+          return Promise.reject({ message: 'User not found', status: 403 })
         }
       })
       .then(json => {
         // only unique user should be able to create user session
         if (json.length !== 1) {
-          return Promise.reject({message: 'User not unique', status: 403})
+          return Promise.reject({ message: 'User not unique', status: 403 })
         } else {
-          request.session.borrowerNumber = json[0].borrowernumber
+          request.session.borrowerNumber = json[ 0 ].borrowernumber
           return fetch('http://xkoha:8081/api/v1/auth/session', {
             method: 'POST',
             headers: {
               'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
               'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
             },
-            body: `userid=${encodeURIComponent(json[0].userid)}&password=${encodeURIComponent(request.body.password)}`
+            body: `userid=${encodeURIComponent(json[ 0 ].userid)}&password=${encodeURIComponent(request.body.password)}`
           })
         }
       })
@@ -36,10 +36,14 @@ module.exports = (app) => {
         if (res.status === 201) {
           request.session.kohaSession = res.headers._headers[ 'set-cookie' ][ 0 ]
           request.session.passwordHash = bcrypt.hashSync(request.body.password)
-          response.send({ isLoggedIn: true, borrowerNumber: request.session.borrowerNumber })
+          return res.json()
         } else {
-          return Promise.reject({message: 'Could not create session', status: 403})
+          return Promise.reject({ message: 'Could not create session', status: 403 })
         }
+      })
+      .then(json => {
+        request.session.borrowerName = `${json.firstname} ${json.surname}`
+        response.send({ isLoggedIn: true, borrowerNumber: request.session.borrowerNumber })
       })
       .catch(error => {
         console.log(error.message)
--- redef/patron-client/src/frontend/utils/dateFormatter.js
+++ redef/patron-client/src/frontend/utils/dateFormatter.js
@@ -1,9 +1,10 @@
-export function formatDate(date) {
+export function formatDate (date) {
   if (date) {
     const formattedDate = new Intl.DateTimeFormat('nb', {
       year: 'numeric',
       month: '2-digit',
-      day: '2-digit'}).format(Date.parse(date.replace(/-/g, '/')))
+      day: '2-digit'
+    }).format(Date.parse(date.replace(/-/g, '/')))
     return formattedDate
   }
 }
