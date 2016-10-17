1fe630f2e906e115b492e67b0bf3e3f7fd1bd0b4
Rurik Greenall <rurik.greenall@gmail.com>
2016-10-17 20:56:21 +0200

    Patron-client: linty linty

--- redef/patron-client/src/frontend/components/PublicationInfo.js
+++ redef/patron-client/src/frontend/components/PublicationInfo.js
@@ -52,13 +52,12 @@ class PublicationInfo extends React.Component {
   }
 
   renderPublisherSeries (publisherSeries) {
-
     if (publisherSeries.length > 0) {
       return (
         publisherSeries.map(serialIssue =>
           <span key="{publisherSeries.id}" data-automation-id="Publication_serial_issue">
             <span data-automation-id="Publication_serial_issue_name">{serialIssue.name}</span>
-            <span data-automation-id="Publication_serial_issue_subtitle">{((serialIssue.subtitle) ? ' — ' + serialIssue.subtitle : '')}</span>
+            <span data-automation-id="Publication_serial_issue_subtitle">{((serialIssue.subtitle) ? ` — ${serialIssue.subtitle}` : '')}</span>
             <span>{': '}</span>
             <span data-automation-id="Publication_serial_issue_issue"> {serialIssue.issue}</span>
           </span>
