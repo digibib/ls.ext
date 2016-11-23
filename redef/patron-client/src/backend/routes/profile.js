c4143d4151f801cf1db900f4f27f2223a30362d9
NichoP <nicho@computas.com>
2016-11-23 10:16:22 +0100

    DEICH-445: Fix indexOf error by coercing number to string

--- redef/patron-client/src/backend/routes/profile.js
+++ redef/patron-client/src/backend/routes/profile.js
@@ -218,7 +218,7 @@ module.exports = (app) => {
       returnVal = 12
     }
 
-    return returnVal
+    return String(returnVal)
   }
 
   function getEligibleItems (items) {
@@ -231,7 +231,7 @@ module.exports = (app) => {
       const from = Date.parse(items[ 0 ].datelastborrowed)
       const to = Date.parse(items[ 0 ].onloan)
       const estimate = Math.ceil((to - from) / secondsInAWeek)
-      return isNaN(estimate) ? 'unknown' : String(estimate)
+      return isNaN(estimate) ? 'unknown' : estimate
     } else {
       return 'unknown'
     }
