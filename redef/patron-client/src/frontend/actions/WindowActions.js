363a5e07b50dce8014a36a608189152eb762c5bc
NichoP <nicho@computas.com>
2016-12-13 10:43:13 +0100

    patron-client: Linting

--- redef/patron-client/src/frontend/actions/WindowActions.js
+++ redef/patron-client/src/frontend/actions/WindowActions.js
@@ -1,10 +1,9 @@
 import { action } from './GenericActions'
 import * as types from '../constants/ActionTypes'
 
-
-export function resizeWindowWidth(windowWidth) {
+export function resizeWindowWidth (windowWidth) {
   return (dispatch, getState) => {
-    if(getState().application.windowWidth !== windowWidth) {
+    if (getState().application.windowWidth !== windowWidth) {
       dispatch(action(types.RESIZE_WINDOW_WIDTH, { windowWidth }))
     }
   }
