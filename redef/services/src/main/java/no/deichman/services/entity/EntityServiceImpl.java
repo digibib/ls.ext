e7941d545044c1fafd96c72cb0ac8d7cd857a9ae
Kristoffer Moe <kristoffer@kodemaker.no>
2017-01-26 14:54:27 +0100

    services: roll back jamon monitoring

--- redef/services/src/main/java/no/deichman/services/entity/EntityServiceImpl.java
+++ redef/services/src/main/java/no/deichman/services/entity/EntityServiceImpl.java
@@ -1,6 +1,5 @@
 package no.deichman.services.entity;
 
-import com.jamonapi.proxy.MonProxyFactory;
 import no.deichman.services.entity.kohaadapter.KohaAdapter;
 import no.deichman.services.entity.kohaadapter.MarcConstants;
 import no.deichman.services.entity.kohaadapter.MarcField;
@@ -704,7 +703,7 @@ public final class EntityServiceImpl implements EntityService {
             log.info("Creating local index for " + type);
             long start = System.currentTimeMillis();
             ResultSet resultSet = repository.retrieveAllNamesOfType(type);
-            nameIndexer = (NameIndexer) MonProxyFactory.monitor(new NameIndexer(resultSet));
+            nameIndexer = new NameIndexer(resultSet);
             if (!nameIndexer.isEmpty()) {
                 nameIndexers.put(type, nameIndexer);
             }
