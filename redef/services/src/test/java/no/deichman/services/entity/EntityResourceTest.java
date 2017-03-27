11933d5d92f1392aeedd86fec9ef9be1305436ff
Rurik Greenall <rurik.greenall@gmail.com>
2017-03-27 12:57:52 +0200

    Services: Update code to match updated Mockito

--- redef/services/src/test/java/no/deichman/services/entity/EntityResourceTest.java
+++ redef/services/src/test/java/no/deichman/services/entity/EntityResourceTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.NotFoundException;
