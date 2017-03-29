c193786200f3aaa571b4abd8a352735a7f5176ec
Rurik Greenall <rurik.greenall@gmail.com>
2017-03-29 16:55:37 +0200

    Services add singleton annotation and make class final (and implement missing context field and  methods)

--- redef/services/src/main/java/no/deichman/services/entity/CirculationResource.java
+++ redef/services/src/main/java/no/deichman/services/entity/CirculationResource.java
@@ -3,10 +3,12 @@ package no.deichman.services.entity;
 import com.google.gson.Gson;
 import no.deichman.services.circulation.CirculationProfile;
 
+import javax.inject.Singleton;
 import javax.servlet.ServletConfig;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import com.google.gson.GsonBuilder;
 import no.deichman.services.entity.kohaadapter.KohaAdapter;
@@ -17,10 +19,14 @@ import static javax.ws.rs.core.Response.ok;
 /**
  * Responsibility: Handle requests for data about circulation.
  */
+@Singleton
 @Path("circulation")
-public class CirculationResource extends ResourceBase {
+public final class CirculationResource extends ResourceBase {
 
     private static final Gson GSON = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().serializeNulls().setPrettyPrinting().create();
+
+    @Context
+    private ServletConfig servletConfig;
 
     public CirculationResource() {}
 
@@ -30,12 +36,14 @@ public class CirculationResource extends ResourceBase {
 
     @GET
     @Path("/{borrowerId: [0-9]+}")
-    public final Response get(@PathParam("borrowerId") String borrowerId) throws Exception {
-        return ok().entity(GSON.toJson(getEntityService().getProfile(borrowerId), CirculationProfile.class)).build();
+    public Response get(@PathParam("borrowerId") String borrowerId) throws Exception {
+        return ok().entity(GSON.toJson(
+                getEntityService().getProfile(borrowerId), CirculationProfile.class)
+        ).build();
     }
 
     @Override
-    protected final ServletConfig getConfig() {
-        return null;
+    protected ServletConfig getConfig() {
+        return servletConfig;
     }
 }
