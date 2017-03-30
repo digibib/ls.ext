36144644cbc7a52314a98401633bc3edd3a010ef
Igor Koudrik <igorkoudrik@gmail.com>
2017-03-30 10:22:14 +0200

    Patron client: suspend reservation fix

--- redef/patron-client/src/frontend/containers/UserLoans.js
+++ redef/patron-client/src/frontend/containers/UserLoans.js
@@ -185,13 +185,14 @@ class UserLoans extends React.Component {
   }
 
   renderResumeSuspendReservationButton (item) {
+    console.log('Change reserve', this.props.isRequestingChangeReservationSuspension)
     return (
       <div>
         {this.props.isRequestingChangeReservationSuspension === item.id
           ? <Loading />
           : (
           <ClickableElement onClickAction={this.props.reservationActions.suspendReservation}
-                            onClickArguments={[ item.reserveId, !item.suspended ]}>
+                            onClickArguments={[ item.id, !item.suspended ]}>
             <button className={`${item.suspended ? 'black-btn red-btn' : 'black-btn'} ${item.queuePlace === '0' ? 'is-hidden' : ''}`}
                     disabled={this.props.isRequestingChangeReservationSuspension !== false}
                     data-automation-id={item.suspended ? 'resume_reservation_button' : 'suspend_reservation_button'}>
