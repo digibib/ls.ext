dc034e24f6edf432f99701da597b001e5ad82535
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2017-04-03 13:27:31 +0200

    patron-client: linting

--- redef/patron-client/src/frontend/components/work/fields/WorkSeries.js
+++ redef/patron-client/src/frontend/components/work/fields/WorkSeries.js
@@ -2,17 +2,16 @@ import React, { PropTypes } from 'react'
 import { injectIntl, intlShape, defineMessages } from 'react-intl'
 import { Link } from 'react-router'
 import MetaItem from '../../MetaItem'
-import isEmpty from '../../../utils/emptyObject'
 import title from '../../../utils/title'
 import fieldQueryLink from '../../../utils/link'
 
 const WorkSeries = ({ workSeries }) => {
-  if (workSeries.length > 0 ) {
+  if (workSeries.length > 0) {
     return (
         <MetaItem label={messages.labelWorkSerie} data-automation-id="work_work_serie">
           {workSeries.map(serie =>
             <span key={serie.id}>
-              <br/>
+              <br />
               <Link
                 data-automation-id="work_series_link"
                 to={fieldQueryLink('serie', serie.mainTitle)}>
@@ -22,7 +21,7 @@ const WorkSeries = ({ workSeries }) => {
             </span>
           )}
         </MetaItem>
-      )
+    )
   }
   return null
 }
