5a094066cb1180ec64b16f120b0772573cf084c2
Rurik Greenall <rurik.greenall@gmail.com>
2017-04-19 15:29:49 +0200

    DEICH-773: catalinker: change some wordings for English language file

--- redef/catalinker/client/src/i18n/en.js
+++ redef/catalinker/client/src/i18n/en.js
@@ -12,7 +12,7 @@
   "use strict"
   return {
     emptyLabel: '',
-    pleaseWait: 'One moment...',
+    pleaseWait: 'Please wait…',
     close: 'Close',
     cancel: 'Cancel',
     proceed: 'Proceed',
@@ -29,35 +29,35 @@
     selectThis: 'Select this',
     autoNumber: 'Automatic numbering',
     searchWorkLegend: 'Search by title, main entry, work uri or work id',
-    searchPublicationLegend: 'Search by title of record id',
+    searchPublicationLegend: 'Search by title or record id',
     advancedSearchLegend: 'You are using <span class="more-info" title="Search containg any of these characters: {{advancedSearchCharacters()}}. You can also use AND, OR og NOT.">advanced search</span>. Press Enter to search.',
     useSuggestion: 'Use suggestion',
     deleteEntry: 'Delete entry',
     editEntry: 'Edit entry',
     defaultAddAnotherLabel: 'Add',
-    addAnotherPageNumber: 'Add a page number',
+    addAnotherPageNumber: 'Add page number',
     addAnotherExtent: 'Add extent',
-    addAnotherISBN: 'Add another ISBN',
-    addAnotherISMN: 'Add another ISMN',
-    addAnotherSeries: 'Add another series',
-    addAnotherRelation: 'Add another relation',
+    addAnotherISBN: 'Add ISBN',
+    addAnotherISMN: 'Add ISMN',
+    addAnotherSeries: 'Add series',
+    addAnotherRelation: 'Add relation',
     addAnotherWorkSeries: 'Add new work series',
-    addAnotherCompositionType: 'Add another composition type',
-    addAnotherInstrument: 'Add another instrument',
-    addAnotherSubject: 'Add another subject',
-    addAnotherClassification: 'Add another classification',
-    addAnotherGenre: 'Add another genre',
-    addAnotherPart: 'Add another part',
-    addAnotherAdditionalEntry: 'Add another additional entry',
-    suggestionFromOtherSources: 'Suggestion from another external source',
-    missingRequiredValuesOfBlankMode: 'Can not add until {{subjectTypeLabelDet(subjectType)}} has been created or loaded. mandatory fields must be yada yada yada.',
-    cannotSelectRelationBeforeResourceTypeIsLoaded: 'Can\'t make this association before relevant resource type has been loaded', //Denne knytningen kan ikke velges før tilhørende ressurstype er lastet
+    addAnotherCompositionType: 'Add composition type',
+    addAnotherInstrument: 'Add instrument',
+    addAnotherSubject: 'Add subject',
+    addAnotherClassification: 'Add classification',
+    addAnotherGenre: 'Add genre',
+    addAnotherPart: 'Add part',
+    addAnotherAdditionalEntry: 'Add contributor',
+    suggestionFromOtherSources: 'Suggestions from external sources',
+    missingRequiredValuesOfBlankMode: 'Cannot add until {{subjectTypeLabelDet(subjectType)}} has been created or loaded. Mandatory fields must populated.',
+    cannotSelectRelationBeforeResourceTypeIsLoaded: 'Cannot select relation until relevant resource type is loaded', //Denne knytningen kan ikke velges før tilhørende ressurstype er lastet
     nameLabel: 'Name', // Navn
     mainEntryLabel: 'Main entry', // Hovedinnførsel
     ISBNLabel: 'ISBN',
     EANLabel: 'EAN',
-    missingMainEntryLabel: 'The work doesn\'t have main responsible', // Verket har ikke hovedansvarlig
-    mainContributorLabel: 'Main responsible', // Hovedansvarlig
+    missingMainEntryLabel: 'Work has no main contributor', // Verket har ikke hovedansvarlig
+    mainContributorLabel: 'Main contributor', // Hovedansvarlig
     agentLabel: 'Agent', // Aktør
     improperWorkLabel: 'Not part of list of works', // Skal ikke inngå i verksliste
     searchWorkAsMainResourceLabel: 'Search for existing work', // Søk etter eksisterende verk
@@ -66,7 +66,7 @@
     seriesTitleLabel: 'Series title', // Serietittel
     seriesPartNumber: 'Series part number', // Nummer på delserie
     issueNumberLabel: 'Number in series', // Nummer i serien
-    subSeriesTitle: 'Sub series title', // Tittel på delserie,
+    subSeriesTitle: 'Sub-series title', // Tittel på delserie,
     workTabLabel: 'Describe work', // Beskriv verk
     workSeriesWorkLabel: 'Work',
     relationTypeLabel: 'Relation type',
@@ -76,7 +76,7 @@
     subjectsTabLabel: 'Subjects', // Emneopplysninger
     instrumentationLabel: 'Intrumentation', // Besetning
     selectSubjectType: 'Select subject type',
-    selectActorType: 'Select actor type',
+    selectActorType: 'Select agent type',
     numberOfPerformarsLabel: 'Number of performers', // Antall utøvere
     classificationLabel: 'Classification', // Klassifikasjon
     classificationSourceLabel: 'Classification source', // Utgave
@@ -85,9 +85,9 @@
     pubPartMainTitleLabel: 'Part title', // Tittel på del
     bulkEntryLinkLabel: 'Add multiple titles',
     bulkEntryLinkToolTip: 'Add multiple parts in one operation', // Åpne mulighet for å legge inn flere titler på én gang'
-    enableBulkEntryLegend: `Enter part titles here separated by carriage return. When you click "Add", one publication part
-                            is created for each title. Empty lines are ignored. Every part is also associated with same agent
-                            and role if entered above. If you do not want autonumbering of parts created, uncheck selection below 
+    enableBulkEntryLegend: `Enter each part-title on a new line. Clicking "Add", creates one publication part
+                            for each title (empty lines are ignored). Every part is also associated with same agent
+                            and role if provided above. If you do not want auto-numbering of parts, uncheck below 
                             `,
 //                          Legg inn titler på delene her med et linjeskift mellom hver. Når du trykker på "Legg til",
 //                          opprettes en utgivelesedel for hver tittel. Tomme linjer blir ignorert. Hver del får også
@@ -156,7 +156,7 @@
 
     // maintenance search field labels
     maintPersonLabelPlur: 'People',
-    maintCorporationLabelPlur : 'Corprations',
+    maintCorporationLabelPlur : 'Corporations',
     maintGeneralSubjectLabelPlur: 'Subjects',
     maintPlaceLabelPlur: 'Places',
     maintEventLabelPlur: 'Events',
@@ -186,14 +186,14 @@
     nextStepDescribeWork: 'Next step: Describe work',
     nextStepSubjects: 'Next step: Subjects',
     nextStepDescribeParts: 'Next step: Describe parts',
-    nextStepAdditionalEntries: 'Next step: Additonal entries',
+    nextStepAdditionalEntries: 'Next step: Additonal contributors',
     finishRegisterPublication: 'Finish',
 
     // other buttons
     deletePublication: 'Delete this publication',
     deleteWork: 'Delete this work',
-    changeWorkOfPublication: 'Replace work of publication',
-    confirmChangeWorkOfPublication: 'Are you sure you wish to change the work relation of this publication?', // Er du sikker på at du vil endre verksknytningen for denne utgivelsen?
+    changeWorkOfPublication: 'Move publication to another work',
+    confirmChangeWorkOfPublication: 'Are you sure you wish to move this publication to another work?', // Er du sikker på at du vil endre verksknytningen for denne utgivelsen?
     extendedEdit: 'Extended editing',
 
     // dialogs
@@ -204,14 +204,14 @@
     publicationDeleted: 'The publication {{title}} of the work {{work}} is deleted',
     deletePublicationFailed: 'Could not delete the publication',
     itemsLeftMessage: 'There are {{numberOfItemsLeft}} items registered in Koha.',
-    gotoKohaForDeleteLink: `Go to <a data-automation-id="biblio_record_link" target="_blank"
+    gotoKohaForDeleteLink: `Remove <a data-automation-id="biblio_record_link" target="_blank"
                href="{{config.kohaIntraUri}}/cgi-bin/koha/catalogue/detail.pl?biblionumber={{publicationId()}}"
-               class="link">the publication there</a> and remove the items first.
+               class="link">items in Koha</a> before continuing.
     `,
 
     // merge authorities
     mergeResourcesDialogTitle: 'Merge authorities',
-    useValuesFromTheseFields: 'Do you wish to use the values from these fields:', // Vil du bruke verdiene fra disse feltene:
+    useValuesFromTheseFields: 'Use values from these fields:', // Vil du bruke verdiene fra disse feltene:
     andFillTheEmptyFields: '...to fill the empty fields for these:', // ...til å fylle inn tomme felter for disse:
 
     // delete authority
@@ -226,13 +226,13 @@
     deleteWorkDialogTitle: 'Delete work',
     cannotDeleteWorkForReferences: 'This work cannot be deleted because there {{references > 1 ? \'are\' : \'is\'}} {{references}} references to it:',
     confirmDeleteWork: 'Confirm delete work',
-    confirmDeleteWorkDetail: 'Yes, I wish to delete this work:',
+    confirmDeleteWorkDetail: 'Yes, I want to delete this work:',
     workDeleted: '{{title}} is deleted',
     workDeleteFailed: 'Could not delete the work',
-    workDeleteFailedPublicationsLeft: 'There {{numberOfPublicationsLeft > 1 ? \'are\' : \'is\'}} {{numberOfPublicationsLeft}} publications of it.',
+    workDeleteFailedPublicationsLeft: 'There {{numberOfPublicationsLeft > 1 ? \'are\' : \'is\'}} {{numberOfPublicationsLeft}} publications linked to it.',
 
     // edit authority
-    confirmEditResource: 'Are you certain you wish to edit {{fieldLabel}} of {{rdfType + \'LabelDet\'}}',
+    confirmEditResource: 'Are you sure you wish to edit {{fieldLabel}} of {{rdfType + \'LabelDet\'}}',
 
     // task descriptions
     filmPub: 'Cataloguing film',
@@ -261,11 +261,11 @@
     compareSerial: 'Compare and merge series',
     compareWorkSeries: 'Compare and merge work series',
     compareInstrument: 'Compare and merge instrument authority',
-    compareCompositionType: 'Compare and merge compisiton type',
+    compareCompositionType: 'Compare and merge composition type',
     compareWork: 'Compare and merge work',
 
     // menu
-    menuHeading: 'Cataloguing and maintenance of the bibliographic collection',
+    menuHeading: 'Cataloguing and maintenance of bibliographic collections',
     newMaterial: 'New material',
     authorityMaintenance: 'Authority maintenance',
 
@@ -280,19 +280,19 @@
     thisPublicationIsAlreadyRegistered: 'This publication is already registered',
     alreadyRegisteredISBNSingular: 'There is already a registered a publication with the same ISBN number. Do you want to open that one, continue registering a new one or cancel?',
     alreadyRegisteredISBNPlural: 'There are already registered {{numberOfResources}} publications with the same ISBN number. Do you want to open one of these, continue registering a new one or cancel?',
-    suggestedPrefilledValues: 'Suggestions for pre-filled values',
+    suggestedPrefilledValues: 'Suggestions for pre-populated values',
 
     // extended editing
-    worksAndPubsRelatedToAuthority: 'Work and publication associated with {{applicationData.translations[applicationData.language][rdfType + \'LabelDet\']}}:',
+    worksAndPubsRelatedToAuthority: 'Works and publications associated with {{applicationData.translations[applicationData.language][rdfType + \'LabelDet\']}}:',
     noHitsInExternalSource: 'No hits in external source',
     searchForDuplicates: 'Search for duplicates',
     noRelations: 'No relations',
-    cannotMergeWithSelf: 'You can\'t merge with the same {{applicationData.translations[applicationData.language][rdfType + \'Label\']}} you are currently editing.',
+    cannotMergeWithSelf: 'You cannot merge with the same {{applicationData.translations[applicationData.language][rdfType + \'Label\']}} you are currently editing.',
     mergeAuthoritiesLegend: `If you chose to merge these to {{applicationData.translations[applicationData.language][rdfType + 'LabelPlur']}} into one, 
                     all the relations that are associated with the {{applicationData.translations[applicationData.language][rdfType + 'Label']}} to the <b>right</b> 
                     are moved to the {{applicationData.translations[applicationData.language][rdfType + 'Label']}} on the <b>left</b>. The information in the fields
-                    to the <b>left</b> remains, whereas the information to the right is deleted. Therefore, please make certain that all relevant information is transferred
-                    from the {{applicationData.translations[applicationData.language][rdfType + 'Label']}} on the <b>right</b> side to the one on the <b>left</b> side 
+                    to the <b>left</b> remain, whereas the information to the right is deleted. Therefore, please make certain that all relevant information is transferred
+                    from the {{applicationData.translations[applicationData.language][rdfType + 'Label']}} on the <b>right</b> to the one on the <b>left</b> 
                     <b>before</b> merging the two {{applicationData.translations[applicationData.language][rdfType + 'LabelPlur']}}.
 `,//
     mergeAuthButtonLabel: 'Merge these two {{applicationData.translations[applicationData.language][rdfType + \'LabelPlur\']}}',
@@ -301,17 +301,17 @@
     showPublicationsAndSplitWork: 'Show publications/split work',
     splitTheWork: 'Split the work',
     splitWork: 'Split work',
-    makeWorkCopyForEachPubliation: 'Make a copy of the work - one for each publication  - and attach the publications to the new works',
+    makeWorkCopyForEachPubliation: 'Make a copy of the work - one for each publication - and attach the publications to the new works',
     workHasNoPublications: 'This work has no publications.',
     workHasNumberOfPublications: 'This work has {{numberOfRelations}} publication{{numberOfRelations > 1 ? \'s\' : \'\'}}:',
     splitWorkLegend: `If you choose to split this work, a copy of it is created for each publication selected above.
-                  Each publication is then attached to its new work as publication of it. Before the work is splitted, you will be asked
-                  to select which values from the publications should be copied to corresponding fields of the new works`,
-    splitWorkDialogLegend: 'Select which values should be copied from existing publications to the new works that will be created:',
+                  Each publication is then attached to its own new work. Before the work is split, you will be asked
+                  to select which values from the publications should be copied to corresponding fields on the new works`,
+    splitWorkDialogLegend: 'Select values to be copied from existing publications to the new works that will be created:',
 
     // links
     showWorkPageLink: 'Work page',
-    showTURTLEForWorkLink: 'Work as TURTLE linked data',
+    showTURTLEForWorkLink: 'Work as TURTLE (linked data)',
     showPublicationInKohaLink: 'Publication in Koha',
     showSummaryLink: 'Catalogue data for this publication',
 
