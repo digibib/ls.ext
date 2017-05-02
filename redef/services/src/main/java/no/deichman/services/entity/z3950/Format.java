d1925dfa42a02e70b7b60584c1faf1bc4c60e863
Asgeir Rekkavik <arekkavik@gmail.com>
2017-05-02 13:45:00 +0200

    Add Nintendo Wii U

--- redef/services/src/main/java/no/deichman/services/entity/z3950/Format.java
+++ redef/services/src/main/java/no/deichman/services/entity/z3950/Format.java
@@ -36,6 +36,7 @@ public final class Format {
             .put("mk", "XboxOneGame")
             .put("mn", "NintendoDSGame")
             .put("mo", "NintendoWiiGame")
+            .put("mo", "NintendoWiiUGame")
             .put("ms", "NintendoSwitchGame")
             .put("na", "PDF")
             .put("nb", "EPUB")
