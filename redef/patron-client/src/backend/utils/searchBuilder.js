eeb6d3c173bc107ebbbd05ba5822c750d99badc8
Kristoffer Moe <kristoffer@kodemaker.no>
2017-05-16 15:21:00 +0200

    patron-client: add explain to work-part of search DEICH-152

--- redef/patron-client/src/backend/utils/searchBuilder.js
+++ redef/patron-client/src/backend/utils/searchBuilder.js
@@ -153,7 +153,8 @@ function initCommonQuery (workQuery, publicationQuery, workFilters, publicationF
           }
         ]
       }
-    }
+    },
+    explain: options.explain === 'true'
   }
 }
 
@@ -308,7 +309,7 @@ module.exports.buildQuery = function (urlQueryString) {
   const workFilters = parseFilters(params.filter || [], 'work')
   const publicationFilters = createPublicationFilters(params, excludeUnavailable)
   const query = queryStringToQuery(params.query, workFilters, publicationFilters, excludeUnavailable, params.page, params.pageSize, params)
-  console.log(JSON.stringify(query, null, 2))
+//  console.log(JSON.stringify(query, null, 2))
   return query
 }
 
