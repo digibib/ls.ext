b2e2d797f2b8bb9ea90f967f8fa6befd5d760679
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2017-06-01 15:18:32 +0200

    patron-client: show age limit
    If ageLimit is same for all publications, show on work level.
    
    Change datatype of ageLimit to xsd:integer in ontology (all our
    data was xsd:integer)

---
+++ redef/patron-client/src/frontend/components/work/fields/WorkAgeLimit.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react'
+import { injectIntl, intlShape, defineMessages, FormattedMessage } from 'react-intl'
+import MetaItem from '../../MetaItem'
+
+const WorkAgeLimit = ({ publications, intl }) => {
+  const ageLimits = [...new Set(publications.map(p => p.ageLimit).filter(l => l))]
+  if (ageLimits.length === 1) {
+    return (
+      <MetaItem label={messages.ageLimit} data-automation-id="work_ageLimit">
+        {ageLimits[0]}
+      </MetaItem>
+    )
+  } else if (ageLimits.length > 1) {
+    return (
+      <MetaItem label={messages.ageLimit} data-automation-id="work_ageLimit">
+          <FormattedMessage {...messages.seePublication} />
+      </MetaItem>
+    )
+  } else {
+    return null
+  }
+}
+
+WorkAgeLimit.defaultProps = {
+  publications: []
+}
+
+WorkAgeLimit.propTypes = {
+  publications: PropTypes.array.isRequired,
+  intl: intlShape.isRequired
+}
+
+export const messages = defineMessages({
+  ageLimit: {
+    id: 'AgeLimit.ageLimit',
+    description: 'Label for age limit meta',
+    defaultMessage: 'Age limit'
+  },
+  seePublication: {
+    id: 'WorkAgeLimit.seePublication',
+    description: 'Label telling you to look for age limits under publication information',
+    defaultMessage: 'See publication'
+  }
+})
+
+export default injectIntl(WorkAgeLimit)
