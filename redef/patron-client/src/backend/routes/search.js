7dbf30a7c0f7e28705cbd86eb86c16b7ae85db84
Kristoffer Moe <kristoffer@kodemaker.no>
2017-06-08 13:15:04 +0200

    patron-client: load filtered and unfiltered aggregations asynchronously DEICH-152

--- redef/patron-client/src/backend/routes/search.js
+++ redef/patron-client/src/backend/routes/search.js
@@ -4,47 +4,41 @@ const searchBuilder = require('../utils/searchBuilder')
 module.exports = (app) => {
   app.get('/q', (request, response) => {
     const queryString = request.originalUrl.substr(request.originalUrl.indexOf('?') + 1)
-    fetch('http://elasticsearch:9200/search/work/_search', {
-      method: 'POST',
-      body: JSON.stringify(searchBuilder.buildUnfilteredAggregatedQuery(queryString))
-    }).then(res => {
-      if (res.ok) {
-        return res.json().then(aggregationResult => {
-          return fetch('http://elasticsearch:9200/search/work/_search', {
-            method: 'POST',
-            body: JSON.stringify(searchBuilder.buildQuery(queryString))
-          }).then(res => {
-            return res.json().then(searchResult => {
-              Object.keys(aggregationResult.aggregations.facets).forEach(facet => {
-                const searchResultFacet = searchResult.aggregations.facets[ facet ]
-                if (typeof searchResultFacet === 'object') {
-                  if (!searchResultFacet) {
-                    searchResult.aggregations.facets[ facet ] = aggregationResult.aggregations.facets[ facet ]
-                  } else {
-                    aggregationResult.aggregations.facets[ facet ].buckets.forEach(aggBucket => {
-                      if (!searchResultFacet.buckets.find(bucket => { return bucket.key === aggBucket.key })) {
-                        searchResultFacet.buckets.push({
-                          key: aggBucket.key,
-                          doc_count: 0,
-                          parents: { value: 0 }
-                        })
-                      }
-                    })
-                  }
-                }
-              })
-              searchResult.aggregations = aggregationResult.aggregations
-              return searchResult
+
+    function mergeAggregation (aggregationResult, searchResult) {
+      Object.keys(aggregationResult.aggregations.facets).forEach(facet => {
+        const searchResultFacet = searchResult.aggregations.facets[ facet ]
+        if (typeof searchResultFacet === 'object') {
+          if (!searchResultFacet) {
+            searchResult.aggregations.facets[ facet ] = aggregationResult.aggregations.facets[ facet ]
+          } else {
+            aggregationResult.aggregations.facets[ facet ].buckets.forEach(aggBucket => {
+              if (!searchResultFacet.buckets.find(bucket => { return bucket.key === aggBucket.key })) {
+                searchResultFacet.buckets.push({
+                  key: aggBucket.key,
+                  doc_count: 0,
+                  parents: { value: 0 }
+                })
+              }
             })
-          })
-        })
-      } else {
-        return res.text().then(error => { return Promise.reject({ message: error, queryString: queryString }) })
-      }
-    }).then(json => response.status(200).send(json))
-      .catch(error => {
-        console.log(`Error in query: ${error.queryString}\n${error.message}`)
-        response.sendStatus(500)
+          }
+        }
       })
+      return searchResult
+    }
+
+    Promise.all([
+      fetch('http://elasticsearch:9200/search/work/_search', {
+        method: 'POST',
+        body: JSON.stringify(searchBuilder.buildUnfilteredAggregatedQuery(queryString))
+      }).then(res => res.json()),
+      fetch('http://elasticsearch:9200/search/work/_search', {
+        method: 'POST',
+        body: JSON.stringify(searchBuilder.buildQuery(queryString))
+      }).then(res => res.json())
+    ]).then(res => {
+      return mergeAggregation(res[ 0 ], res[ 1 ])
+    }).then(json => response.status(200).send(json))
+      .catch(error => { response.status(500).send({ message: error, queryString: queryString }) })
   })
 }
