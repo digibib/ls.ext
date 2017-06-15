4dbd585768b2bb1f7157c8a6184271b5bbbfea3d
Kristoffer Moe <kristoffer@kodemaker.no>
2017-06-15 16:29:00 +0200

    services: simple phrase permutator and adjust score function DEICH-152

--- redef/patron-client/src/backend/utils/queryConstants.js
+++ redef/patron-client/src/backend/utils/queryConstants.js
@@ -9,8 +9,8 @@ module.exports = {
     }
   },
   defaultPublicationFields: [
-    { field: 'agents', boost: 10 },
-    { field: 'author', boost: 30 },
+    { field: 'agents', boost: 5 },
+    { field: 'author', boost: 5 },
     { field: 'country' },
     { field: 'desc' },
     { field: 'ean' },
@@ -19,18 +19,20 @@ module.exports = {
     { field: 'isbn' },
     { field: 'ismn' },
     { field: 'language' },
-    { field: 'mainTitle', boost: 50 },
+    { field: 'mainTitle', boost: 50, phrase: true },
     { field: 'mainTitle.raw', boost: 100 },
+    { field: 'subtitle', boost: 50, phrase: true },
+    { field: 'subtitle.raw', boost: 100 },
     { field: 'mt' },
-    { field: 'partTitle' },
+    { field: 'partTitle', phrase: true },
     { field: 'publishedBy' },
     { field: 'recordId' },
-    { field: 'series' },
+    { field: 'series', boost: 10 },
     { field: 'title', boost: 20 }
   ],
   defaultWorkFields: [
-    { field: 'agents', boost: 10 },
-    { field: 'author', boost: 30 },
+    { field: 'agents', boost: 5 },
+    { field: 'author', boost: 5 },
     { field: 'bio' },
     { field: 'compType' },
     { field: 'country' },
@@ -39,13 +41,14 @@ module.exports = {
     { field: 'inst' },
     { field: 'language' },
     { field: 'litform' },
-    { field: 'mainTitle', boost: 5 },
+    { field: 'mainTitle', boost: 5, phrase: true },
     { field: 'mainTitle.raw', boost: 5 },
+    { field: 'subtitle', boost: 5, phrase: true },
+    { field: 'subtitle.raw', boost: 5 },
     { field: 'mt' },
-    { field: 'partTitle' },
-    { field: 'subtitle' },
-    { field: 'series' },
-    { field: 'subject', boost: 1 },
-    { field: 'title', boost: 20 }
+    { field: 'partTitle', phrase: true },
+    { field: 'series', boost: 10 },
+    { field: 'subject', boost: 1, phrase: true },
+    { field: 'title', boost: 5 }
   ]
 }
