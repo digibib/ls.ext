8443f6d589c71dea6e6d029f2163ad0d832a2913
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2017-08-28 11:18:16 +0200

    docker-compose: remove overview

--- redef/overview/files/generate_index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# simple entrypoint that substitutes ENV vars in html template and generate index.html
-eval "cat <<EOF
-$(cat /src/overview.tpl)
-EOF
-" 2> /dev/null > /etc/nginx/html/index.html
-
-# run nginx in foreground
-nginx -g "daemon off;"
