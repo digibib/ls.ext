8ff8555ad50eee2dfdf36393cc39edddb8971773
Benjamin Rokseth <bensinober@gmail.com>
2017-10-09 15:16:58 +0200

    Update Koha to 17.05.04 - various security fixes

--- test/features/support/hooks.rb
+++ test/features/support/hooks.rb
@@ -63,7 +63,7 @@ Before do |scenario|
     # Pre-populate Koha once before tests are run
     unless $kohadb_setup
       $kohadb_setup = TestSetup::Koha.new "xkoha"
-      $kohadb_setup.setup_db "17.0501000"
+      $kohadb_setup.setup_db "17.0504000"
     end
     @context[:koha] = $kohadb_setup
   end
