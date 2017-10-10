8d9a1d2dfa68c2f44439c8fbbff2fe47cec1ff59
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2017-10-10 11:35:31 +0200

    patron-client: show untranscribed if match on work DEICH-911

--- redef/patron-client/src/frontend/utils/searchResponseParser.js
+++ redef/patron-client/src/frontend/utils/searchResponseParser.js
@@ -48,7 +48,7 @@ export function processSearchResponse (response, locationQuery) {
       result.contributors.forEach(contributor => {
         contributor.agent.relativeUri = relativeUri(contributor.agent.uri)
       })
-      result.untranscribedTitle = element._source.untranscribedTitle
+      result.workUntranscribedTitle = element._source.untranscribedTitle
 
       if (element.inner_hits.publications.hits.hits.length > 0) {
         result.formats = [].concat.apply([], element.inner_hits.publications.hits.hits.map(pub => { return pub._source.formats || [] }))
@@ -131,7 +131,7 @@ export function processSearchResponse (response, locationQuery) {
           partNumber: selected._source.partNumber,
           partTitle: selected._source.partTitle
         })
-        result.untranscribedTitle = result.untranscribedTitle || selected._source.untranscribedTitle
+        result.untranscribedTitle = selected._source.untranscribedTitle
         if (selected._source.uri) {
           result.relativePublicationUri = `${result.relativeUri}${relativeUri(selected._source.uri)}`
         } else {
@@ -161,10 +161,17 @@ export function processSearchResponse (response, locationQuery) {
         })
       }
 
-      if (element._source.untranscribedTitle && !titleMatchTerms.every(term => { return result.untranscribedTitle.toLocaleLowerCase().includes(term) })) {
+      if (result.workUntranscribedTitle) {
         // We only want to show untranscribed title from work if there is a match in the search terms
-        result.untranscribedTitle = false
+        if (titleMatchTerms.length === 0 || !titleMatchTerms.every(term => { return result.workUntranscribedTitle.toLocaleLowerCase().includes(term) })) {
+          result.workUntranscribedTitle = false
+        }
+
+        if (!result.untranscribedTitle) {
+          result.untranscribedTitle = result.workUntranscribedTitle
+        }
       }
+
 
       result.mediaTypes = [ ...new Set(element.inner_hits.publications.hits.hits.filter(pub => pub._source.mediatype).map(pub => (pub._source.mediatype))) ].map(uri => ({ uri }))
 
