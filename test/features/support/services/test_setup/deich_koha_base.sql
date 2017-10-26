3ff4631352cf4ae5fce338bc126758ca16917340
Benjamin Rokseth <bensinober@gmail.com>
2017-10-26 13:24:16 +0200

    Tests: Add autoincrement fix to Koha setup

--- test/features/support/services/test_setup/deich_koha_base.sql
+++ test/features/support/services/test_setup/deich_koha_base.sql
@@ -659,6 +659,39 @@ INSERT INTO `overduerules_transport_types` VALUES (304,1,'email',11),(305,1,'sms
 /*!40000 ALTER TABLE `overduerules_transport_types` ENABLE KEYS */;
 UNLOCK TABLES;
 
+-- Fix corrupted autoincrement values by setting to highest existing ID
+-- https://wiki.koha-community.org/wiki/DBMS_auto_increment_fix
+
+SET @new_AI_borrowers = ( SELECT GREATEST( IFNULL( ( SELECT MAX(borrowernumber) FROM borrowers ), 0 ), IFNULL( ( SELECT MAX(borrowernumber) FROM deletedborrowers ), 0 ) ) + 1 );
+SET @sql = CONCAT( 'ALTER TABLE borrowers AUTO_INCREMENT = ', @new_AI_borrowers );
+PREPARE st FROM @sql;
+EXECUTE st;
+
+SET @new_AI_biblio = ( SELECT GREATEST( IFNULL( ( SELECT MAX(biblionumber) FROM biblio ), 0 ), IFNULL( ( SELECT MAX(biblionumber) FROM deletedbiblio ), 0 ) ) + 1 );
+SET @sql = CONCAT( 'ALTER TABLE biblio AUTO_INCREMENT = ', @new_AI_biblio );
+PREPARE st FROM @sql;
+EXECUTE st;
+
+SET @new_AI_biblioitems = ( SELECT GREATEST( IFNULL( ( SELECT MAX(biblioitemnumber) FROM biblioitems ), 0 ), IFNULL( ( SELECT MAX(biblioitemnumber) FROM deletedbiblioitems ), 0 ) ) + 1 );
+SET @sql = CONCAT( 'ALTER TABLE biblioitems AUTO_INCREMENT = ', @new_AI_biblioitems );
+PREPARE st FROM @sql;
+EXECUTE st;
+
+SET @new_AI_items = ( SELECT GREATEST( IFNULL( ( SELECT MAX(itemnumber) FROM items ), 0 ), IFNULL( ( SELECT MAX(itemnumber) FROM deleteditems ), 0 ) ) + 1 );
+SET @sql = CONCAT( 'ALTER TABLE items AUTO_INCREMENT = ', @new_AI_items );
+PREPARE st FROM @sql;
+EXECUTE st;
+
+SET @new_AI_issues = ( SELECT GREATEST( IFNULL( ( SELECT MAX(issue_id) FROM issues ), 0 ), IFNULL( ( SELECT MAX(issue_id) FROM old_issues ), 0 ) ) + 1 );
+SET @sql = CONCAT( 'ALTER TABLE issues AUTO_INCREMENT = ', @new_AI_issues );
+PREPARE st FROM @sql;
+EXECUTE st;
+
+SET @new_AI_reserves = ( SELECT GREATEST( IFNULL( ( SELECT MAX(reserve_id) FROM reserves ), 0 ), IFNULL( ( SELECT MAX(reserve_id) FROM old_reserves ), 0 ) ) + 1 );
+SET @sql = CONCAT( 'ALTER TABLE reserves AUTO_INCREMENT = ', @new_AI_reserves );
+PREPARE st FROM @sql;
+EXECUTE st;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -671,8 +704,3 @@ UNLOCK TABLES;
 
 -- Dump completed on 2016-11-18 12:19:13
 
-TRUNCATE TABLE `issues`;
-ALTER TABLE `issues` AUTO_INCREMENT = 1;
-
-TRUNCATE TABLE `old_issues`;
-ALTER TABLE `old_issues` AUTO_INCREMENT = 1;
