fbae38f75d762bc88b44452f442b814597de9680
Benjamin Rokseth <bensinober@gmail.com>
2017-11-01 19:02:19 +0100

    patron-client: Remote reservation constraint - fix error message

--- redef/patron-client/src/frontend/actions/ReservationActions.js
+++ redef/patron-client/src/frontend/actions/ReservationActions.js
@@ -323,7 +323,7 @@ export function remoteReservePublication (recordId, userId, reservationComment) 
         return dispatch(remoteReservePublicationSuccess(recordId, userId))
       } else if (response.status === 400) {
         return response.json().then(err => {
-          if (err === 'Material not approved for remote reserval') {
+          if (err.error === 'Material not approved for remote reserval') {
             throw Error(Errors.reservation.MATERIAL_NOT_AVAILABLE_FOR_REMOTE_RESERVE)
           } else {
             throw Error(Errors.reservation.GENERIC_RESERVATION_ERROR)
