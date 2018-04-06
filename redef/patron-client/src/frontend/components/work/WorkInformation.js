fd8460a55a691fb2040dda298f5c860090b189c8
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2018-04-06 14:46:16 +0200

    Revert "patron-client: remove author heading from work page"
    This reverts commit a8ecdf4603ea85f8659cfba8e7bd4ba7f9917536.

--- redef/patron-client/src/frontend/components/work/WorkInformation.js
+++ redef/patron-client/src/frontend/components/work/WorkInformation.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import MediaQuery from 'react-responsive'
 
+import Author from './fields/Author'
 import Year from './fields/Year'
 import AdditionalInformation from './AdditionalInformation'
 import Summary from './fields/Summary'
@@ -26,6 +27,7 @@ const WorkInformation = ({ work, publicationId, showAdditionalInformation, toggl
   return (
     <section className="work-information">
       <Title title={publicationTitle || originalTitle} untranscribedTitle={untranscribedPublicationtitle} />
+      <Author by={work.by} />
       <OriginalTitle title={originalTitle} untranscribedTitle={untranscribedOriginalTitle} />
       <OriginalLanguage languages={work.languages} />
       <CountryOfOrigin country={work.countryOfOrigin} />
