b23c1865a1684aaf2850824a7629edb04ca4ac21
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2018-05-23 12:45:05 +0200

    patron-client: hide/show part of opt in message
    Final text pending.

--- redef/patron-client/src/frontend/components/OptInHistoryInfo.js
+++ redef/patron-client/src/frontend/components/OptInHistoryInfo.js
@@ -3,19 +3,29 @@ import { defineMessages, FormattedMessage, FormattedHTMLMessage } from 'react-in
 
 class OptInHistoryInfo extends React.Component {
 
+  toggleSeeMore () {
+    document.getElementById('see-more').classList.toggle('hidden')
+  }
+
   render () {
     if (this.props.hasHistory) {
       return (
           <div>
             <h1><FormattedMessage {...messages.saveMyHistoryRevivalHeader} /></h1>
-            <p><FormattedHTMLMessage {...messages.saveMyHistoryRevivalInformation} /></p>
+            <div><FormattedHTMLMessage {...messages.saveMyHistoryRevivalInformation} /></div>
+            <a className="see-more-link" onClick={this.toggleSeeMore}><FormattedMessage {...messages.saveMyHistoryReadMoreLink} /></a>
+            <div id="see-more" className="hidden"><FormattedHTMLMessage {...messages.saveMyHistoryReadMoreText} /></div>
+            <p><FormattedHTMLMessage {...messages.saveMyHistoryDeclaration} /></p>
           </div>
       )
     }
     return (
         <div>
           <h1><FormattedMessage {...messages.saveMyHistoryHeader} /></h1>
-          <p><FormattedHTMLMessage {...messages.saveMyHistoryInformation} /></p>
+          <div><FormattedHTMLMessage {...messages.saveMyHistoryInformation} /></div>
+          <a className="see-more-link" onClick={this.toggleSeeMore}><FormattedMessage {...messages.saveMyHistoryReadMoreLink} /></a>
+          <div id="see-more" className="hidden"><FormattedHTMLMessage {...messages.saveMyHistoryReadMoreText} /></div>
+          <p><FormattedHTMLMessage {...messages.saveMyHistoryDeclaration} /></p>
         </div>
     )
   }
@@ -45,6 +55,21 @@ export const messages = defineMessages({
     id: 'History.saveMyHistoryRevivalInformation',
     description: 'The text for save my history consent for users opted in in previous system',
     defaultMessage: 'You will have a full history of what you have ...'
+  },
+  saveMyHistoryDeclaration: {
+    id: 'History.saveMyHistoryDeclaration',
+    description: 'Text text with link to full privacy declaration',
+    defaultMessage: 'Read our <a href="https://www.deichman.no/node/20992">privacy declaration</a>.'
+  },
+  saveMyHistoryReadMoreLink: {
+    id: 'History.saveMyHistoryReadMoreLink',
+    description: 'Link text for expanding details',
+    defaultMessage: 'Read more'
+  },
+  saveMyHistoryReadMoreText: {
+    id: 'History.saveMyHistoryReadMoreText',
+    description: 'Text text with link to full privacy declaration',
+    defaultMessage: 'TODO'
   }
 })
 
