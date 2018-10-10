902897c36d5c2dd3e074cdce18fc23b40c3e8665
Benjamin Rokseth <benjamin.rokseth@deichman.no>
2018-10-10 19:53:41 +0200

    services: remove mysql client from Dockerfile

--- redef/services/entrypoint.dev.sh
+++ redef/services/entrypoint.dev.sh
@@ -1,28 +1,5 @@
 #!/usr/bin/env bash
 
-# wait until database is responding and has been populated by koha web installer
-while true; do
-  mysql --protocol=tcp -h koha_mysql -u"$KOHA_MYSQL_USER" -p"$KOHA_MYSQL_PASS" "$KOHA_MYSQL_DB" -e "SELECT 1 FROM categories,branches,borrowers LIMIT 1"
-  if [ $? = 0 ]; then break; fi;
-  sleep 3s;
-done;
-
-set -e
-
-# Setup Koha REST api user
-# ------------------------
-# Add patron categories for API user and Self registrated users
-mysql --protocol=tcp -h koha_mysql -u"$KOHA_MYSQL_USER" -p"$KOHA_MYSQL_PASS" "$KOHA_MYSQL_DB" -e "\
-  INSERT IGNORE INTO categories(categorycode, description, enrolmentperiod, upperagelimit, category_type) VALUES \
-  ('API', 'API-user', NULL, NULL, 'S'), \
-  ('REGVOKSEN', 'Selvregistrert voksen', 1, NULL, 'A'), \
-  ('REGBARN', 'Selvregistrert barn', 1, 16, 'C')"
-# Add branch for API user
-mysql --protocol=tcp -h koha_mysql -u"$KOHA_MYSQL_USER" -p"$KOHA_MYSQL_PASS" "$KOHA_MYSQL_DB" -e "INSERT IGNORE INTO branches(branchcode, branchname) VALUES ('api', 'Intern API avdeling')"
-# Add user with permissions needed for API use
-mysql --protocol=tcp -h koha_mysql -u"$KOHA_MYSQL_USER" -p"$KOHA_MYSQL_PASS" "$KOHA_MYSQL_DB" -e "INSERT IGNORE INTO borrowers(surname,branchcode,cardnumber,userid,password,flags,categorycode,privacy,dateexpiry) VALUES ('$KOHA_API_USER', 'api', '$KOHA_API_USER','$KOHA_API_USER','$KOHA_API_PASS_ENCRYPTED',1,'API',1,'2199-12-31')"
-
-
 # Javarunner
 # ----------
 
